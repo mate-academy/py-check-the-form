8894c79d766153c8271ac7ab4713491848eca08d
Denys Lipin <lipin.transstream@gmail.com>
2022-02-04 22:26:23 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,37 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password,result", [
+        ("Small@1", False),
+        ("Very_nice@123!", True),
+        ("To_Big_Password_123@!", False)
+    ]
+)
+def test_should_check_length_of_password(password, result):
+    assert check_password(password) is result
+
+
+@pytest.mark.parametrize(
+    "password,result", [
+        ("p@ssword_123", False),
+        ("PassWord123", False),
+        ("Pass@Word_", False),
+        ("Pass@Word_123!", True)
+    ]
+)
+def test_should_check_at_least_1_digit_1_special_char_1_uppercase_letter(
+        password, result):
+    assert check_password(password) is result
+
+
+@pytest.mark.parametrize(
+    "password,result", [
+        ("P@ssword_123/{}*+-", False),
+        ("Pass123$@#&!-_", True)
+    ]
+)
+def test_should_check_special_character(password, result):
+    assert check_password(password) is result
