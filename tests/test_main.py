4b8a4254db460e62b38a40d983f25a3af11cb0fd
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-02-14 18:09:56 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,3 +15,11 @@ def test_deprecated_symbols():
 
 def test_is_necessary_symbols_included():
     assert check_password("1@Password") is True
+
+
+def test_without_capital_letters():
+    assert check_password("qwertyqwerty") is False
+
+
+def test_without_special_symbols():
+    assert check_password("password") is False
