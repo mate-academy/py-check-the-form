a1d37602b866bc348cc9727b50421e8e1707e6ee
Marko Khodan <khodanmarko@gmail.com>
2022-05-30 16:29:20 +0300

    request fixed

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,26 +3,25 @@ from app.main import check_password
 
 class TestCheckPassword:
     def test_should_return_false_when_less_then_8_characters(self):
-        assert not check_password('Qwert@1')
-        assert check_password('Qwert@12')
+        assert check_password('Qwert@1') is False
+        assert check_password('Qwert@12') is True
 
     def test_should_return_false_when_more_then_16_characters(self):
-        assert not check_password('IFqwerty123A$357@')
-        assert check_password('IFqwerty123A$357')
+        assert check_password('IFqwerty123A$357@') is False
+        assert check_password('IFqwerty123A$357') is True
 
     def test_should_return_false_if_no_digit(self):
-        assert not check_password("Qwerty@$a")
-        assert check_password("Qwerty@$a1")
+        assert check_password("Qwerty@$a") is False
+        assert check_password("Qwerty@$a1") is True
 
     def test_should_return_false_if_no_special_character(self):
-        assert not check_password("Qwerty123a")
-        assert check_password("Qwerty123$")
+        assert check_password("Qwerty123a") is False
+        assert check_password("Qwerty123$") is True
 
     def test_should_return_false_if_no_uppercase_letter(self):
-        assert not check_password("qwerty123$")
-        assert check_password("Qwerty123$")
+        assert check_password("qwerty123$") is False
+        assert check_password("Qwerty123$") is True
 
     def test_should_return_false_when_use_not_allowed_symbols(self):
-        assert not check_password("Qwerty123^$")
-        assert check_password("Qwerty123$")
-
+        assert check_password("Qwerty123^$") is False
+        assert check_password("Qwerty123$") is True
