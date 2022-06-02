aef9e2441722953cdbe9a63f396b6c98fd8185fd
Yurii Kindrat <yurkinkek@gmail.com>
2022-06-02 19:12:59 +0300

    Solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,30 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+class TestMain:
+
+    @pytest.mark.parametrize(
+        "password_for_check, func_returns",
+        [
+            pytest.param("Pass1@", False,
+                         id="len of password must be 8-16 symbols"),
+            pytest.param("Pass1@svvhrtxweqcgcrwtgwceg", False,
+                         id="len of password must be 8-16 symbols"),
+            pytest.param("пароль1@", False,
+                         id="password must consist only Latin alphabet"),
+            pytest.param("password1@", False,
+                         id="password should have uppercase letter"),
+            pytest.param("Password1", False,
+                         id="password should have at"
+                            " least 1 special character"),
+            pytest.param("Password@", False,
+                         id="password should have at least 1 digit"),
+            pytest.param("Pass@word1", True,
+                         id="password should have uppercase letter")
+        ]
+    )
+    def test_check_password(self, password_for_check, func_returns):
+
+        assert check_password(password_for_check) == func_returns
