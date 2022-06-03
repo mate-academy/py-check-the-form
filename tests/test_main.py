3b99e30ec7c25cc7c782e233d78e7b696d678c24
Олег Белый <belochard@gmail.com>
2022-06-03 14:31:19 +0300

    Solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,36 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    'password, answer',
+    [
+        pytest.param(
+            'notapasswordatallhonestly',
+            False,
+            id="Too long password"
+        ),
+        pytest.param(
+            'pwd',
+            False,
+            id="Too short password"
+        ),
+        pytest.param(
+            'NoSpecSymb',
+            False,
+            id="No special symbol"
+        ),
+        pytest.param(
+            "8Symbol!",
+            True,
+            id="Exactly 8 symbols"
+        ),
+        pytest.param(
+            "16Symbols!!!!!!",
+            True,
+            id="Exactly 16 symbols"
+        )
+    ]
+)
+def test_check_password(password, answer):
+    assert check_password(password) == answer
