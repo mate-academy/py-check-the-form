8542fcbada8ab4b3caf4a543c201db3560a2bba7
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-06-08 23:42:08 +0300

    Add test for empty string

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,7 +12,8 @@ from app.main import check_password
         pytest.param("@assword1", False, id="Missing uppercase symbol"),
         pytest.param("Pass@wo", False, id="Length password less 8 symbol"),
         pytest.param("Pass@word555ewews", False, id="Length password more 16 symbol"),
-        pytest.param("Pass word1", False, id="Whitespace in password")
+        pytest.param("Pass word1", False, id="Whitespace in password"),
+        pytest.param("", False, id="String with password is empty")
     ]
 )
 def test_check_password(password, result):
