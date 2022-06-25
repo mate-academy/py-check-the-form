d77d7d749f0d42048374c7da32cef6428f3089d4
Artem Kazakov <artemkazakov947@gmail.com>
2022-06-25 13:34:34 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,18 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize("is_valid, result", [
+                         pytest.param("Pass@word1", True,
+                                      id="all requirements complete"),
+                         pytest.param("qwerty", False,
+                                      id="has no digit, special, short pass"),
+                         pytest.param("Str@ng", False,
+                                      id="has no digit, short password"),
+                         pytest.param("qqqqqqqqqqqqqqqqqqqqqqqqqqqqq", False,
+                                      id="too long password"),
+                         pytest.param("вамик", False,
+                                      id="non_latin_letter")]
+                         )
+def test_is_valid_password(is_valid, result):
+    assert check_password(is_valid) == result
