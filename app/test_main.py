bae3615869aedcdb8c8f21322d1a6864d6dbc8bc
anatomst <www.shpachyk98@gmail.com>
2022-07-07 23:44:37 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,36 @@
-# write your code here
+from app.main import check_password
+import pytest
+
+
+@pytest.mark.parametrize(
+    "checked_password, expected",
+    [
+        pytest.param(
+            "Pass@word1", True,
+            id="password with letters, special symbols and numbers"
+        ),
+        pytest.param(
+            "qwerty", False,
+            id="less than 8 characters"
+        ),
+        pytest.param(
+            "Str@ng", False,
+            id="less than 8 characters"
+        ),
+        pytest.param(
+            "passwordutg", False,
+            id="without uppercase and special symbols"
+        ),
+        pytest.param(
+            "Strongpass", False,
+            id="without digits"
+        ),
+        pytest.param(
+            "Strqwertyqwertylongqwert5432ij", False,
+            id="long password"
+        ),
+    ]
+)
+def test_password(checked_password, expected):
+    assert check_password(checked_password) == expected
+    
