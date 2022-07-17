84295a1e52bc857aa1ba083dcd75991926ac0b5a
Denis <sairustv228@gmail.com>
2022-07-17 12:52:01 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -11,7 +11,8 @@ from app.main import check_password
         pytest.param("Pass@ord", False, id="Missing number symbol"),
         pytest.param("@assword1", False, id="Missing uppercase symbol"),
         pytest.param("Pass@wo", False, id="Length password less 8 symbol"),
-        pytest.param("Pass@word555ewews", False, id="Length password more 16 symbol"),
+        pytest.param("Pass@word555ewews", False,
+                     id="Length password more 16 symbol"),
         pytest.param("Pass word1", False, id="Whitespace in password"),
         pytest.param("", False, id="String with password is empty")
     ]
