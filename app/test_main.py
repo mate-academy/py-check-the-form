81d5cde252eb45b125b61746ca2362ca6c5e6ea9
Inna Kushnir <innakushnir63@gmail.com>
2022-08-04 22:37:26 +0300

    Solutions

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+from app.check_password import check_password
+
+
+def test_required_length_of_password():
+    password_ = check_password("Victory1!")
+    assert password_ is True
+
+
+def test_uppercase_letter_in_password():
+    password_ = check_password("korsica1!")
+    assert password_ is False
+
+
+def test_special_character_in_password():
+    password_ = check_password("World1245@")
+    assert password_ is True
+
+
+def test_digits_in_password():
+    password_ = check_password("World@@@@")
+    assert password_ is False
