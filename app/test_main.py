5a3d75799240acdeea80c95ae6ea8c8e01d5c942
stoveprofi <stoveprofi@gmail.com>
2022-08-13 12:35:54 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+from app.main import check_password
+
+
+def test_should_return_false_if_password_less_8_letters():
+    assert check_password("1Dss$") is False
+
+
+def test_should_return_false_if_password_more_16_letters():
+    assert check_password("1Dsrtf6dg#jfhhjfhhs$") is False
+
+
+def test_should_return_true_if_password_is_correcting():
+    assert check_password("1Dsfkophs$") is True
+
+
+def test_return_false_if_passwords_without_uppercase_letter():
+    assert check_password("1sfkophs$") is False
+
+
+def test_return_false_if_passwords_without_uppercase_digits():
+    assert check_password("sfDkophs$") is False
+
+
+def test_return_false_if_passwords_without_uppercase_simbols():
+    assert check_password("sfGDkophs") is False
