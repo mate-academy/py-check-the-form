4655e0ccba91db1df0dbc31bb98587162fb298b1
Diliaver Yusupov <diliaver795@gmail.com>
2022-08-23 11:12:38 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,29 @@
-# write your code here
+from app.main import check_password
+
+
+def test_length_7():
+    assert check_password("1D2d3e!") is False
+
+
+def test_length_8():
+    assert check_password("1D2d3e!8")
+
+
+def test_length_16():
+    assert check_password("1D2d3e!81D2d3e!8")
+
+
+def test_length_17():
+    assert check_password("1D2d3e!81D2d3e!8l") is False
+
+
+def test_all_lower():
+    assert check_password("1d2d3e!8") is False
+
+
+def test_no_number():
+    assert check_password("dDdDeE!t") is False
+
+
+def test_no_special_symbol():
+    assert check_password("dDdDeE1t") is False
