10debaaef3a10ded2f820b7d81123e0ca84a3a8c
Oleg Usenko <Oleg911Usenko@gmail.com>
2022-09-01 21:37:47 +0300

    Task Complete

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+class TestCheckPassword:
+    @pytest.mark.parametrize(
+        "password, result",
+        [
+            ("qw!z4rE", False),
+            ("qwertyIstoolon!gpas85sword", False),
+            ("qw5erty!#", False),
+            ("Pass@!word", False),
+            ("Pas99leg", False),
+        ]
+    )
+    def test_check_password(self, password, result):
+        assert check_password(password) == result
