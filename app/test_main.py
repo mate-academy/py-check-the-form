49439691c3fc795d28a9833f1d25170199f30142
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-09-05 18:18:46 -0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,36 @@
-# write your code here
+from app.main import check_password
+
+
+def test_password_right():
+    password = check_password("NewBrunswick73!@")
+    assert password
+
+
+def test_7_char():
+    password = check_password("Cana73!")
+    assert password is False
+
+
+def test_17_char():
+    password = check_password("NewBrunswick73!@K")
+    assert password is False
+
+
+def test_no_upper():
+    password = check_password("canada73!")
+    assert password is False
+
+
+def test_no_digit():
+    password = check_password("New_canada!")
+    assert password is False
+
+
+def test_no_special_char():
+    password = check_password("canada73")
+    assert password is False
+
+
+def test_empty_string():
+    password = check_password("")
+    assert password is False
