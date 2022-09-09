3a4c2fcbb1bfe7e0e2d8874014313c115b79a8c5
Anton Lasko <anton.lasko1993@gmail.com>
2022-09-09 15:13:10 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+from app.main import check_password
+
+
+def test_when_everything_is_ok():
+    pswrd = check_password('Pass@word1')
+    assert pswrd is True
+
+
+def test_when_length_is_below_eight():
+    pswrd = check_password('Ps@wo1')
+    assert pswrd is False
+
+
+def test_when_length_is_upon_sixteen():
+    pswrd = check_password('Pass@word1shouldbeOK')
+    assert pswrd is False
+
+
+def test_when_no_uppercase():
+    pswrd = check_password('pass@word1')
+    assert pswrd is False
+
+
+def test_when_no_digits():
+    pswrd = check_password('Pass@word')
+    assert pswrd is False
