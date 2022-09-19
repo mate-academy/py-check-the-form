a48d14bd09ac26be3404a9a23a0cc5b73f1db0ec
Juliya Herasymniuk <juliya.gerasimnjyk@gmail.com>
2022-09-19 18:38:45 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,29 @@
-# write your code here
+from app.main import check_password
+
+
+def test_empty_string():
+    assert check_password("") is False
+
+
+def test_short_password():
+    assert check_password("123Qw_") is False
+
+
+def test_long_password():
+    assert check_password("123qwAe123qw@Ae1239") is False
+
+
+def test_no_upper():
+    assert check_password("mateacademy3_") is False
+
+
+def test_no_digit():
+    assert check_password("MateAcademy_") is False
+
+
+def test_no_special_character():
+    assert check_password("MateAcademy3") is False
+
+
+def test_valid_password():
+    assert check_password("MateAcademy3_") is True
