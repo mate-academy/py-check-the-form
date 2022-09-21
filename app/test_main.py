41f4909e7692cdd22a60c83e2e3d0fb9400b9f1b
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-09-21 18:59:10 +0300

    py_check_password_Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+from app.main import check_password
+
+
+def test_for_too_short_password():
+    assert check_password("Passw_1") is False, (
+        "Password should contain minimum 8 characters.")
+
+
+def test_for_too_long_password():
+    assert check_password("Password_test1970") is False, (
+        "Password should contain maximum 16 characters.")
+
+
+def test_should_contain_upper_letter():
+    assert check_password("password_test1") is False, (
+        "Password should contain at least 1 upper character.")
+
+
+def test_should_contain_digit():
+    assert check_password("Password_test") is False, (
+        "Password should contain at least 1 digit.")
+
+
+def test_should_contain_special_character():
+    assert check_password("Passwordtest1") is False, (
+        "Password should contain at least 1 special character.")
