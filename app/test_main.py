43b5c8dfb907da3f289e802b8d8eb590606284a6
mltdll <ilya.vyplavin@gmail.com>
2022-09-25 19:44:16 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,47 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected_output",
+    [
+        pytest.param(
+            "Pass@word1",
+            True,
+            id="should return `True` when password='Pass@word1'"
+        ),
+        pytest.param(
+            "7Char$$",
+            False,
+            id="should return `False` when password='7Char$$'"
+        ),
+        pytest.param(
+            "17_Chars_string#!",
+            False,
+            id="should return `False` when password='17_Chars_string#!'"
+        ),
+        pytest.param(
+            "NoDigit$string",
+            False,
+            id="should return `False` when password='NoDigit$string'"
+        ),
+        pytest.param(
+            "no-upp3rcs$e",
+            False,
+            id="should return `False` when password='no_upp3rcs$e'"
+        ),
+        pytest.param(
+            "NoSpec1alChrs",
+            False,
+            id="should return `False` when password='NoSpec1alChrs'"
+        ),
+        pytest.param(
+            "Unsupp0rted©hr",
+            False,
+            id="should return `False` when password='Unsupp0rted©hr'"
+        )
+    ]
+)
+def test_check_password(password, expected_output):
+    assert check_password(password) is expected_output
