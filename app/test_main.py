84b984873c61ab4017f6d36cd2951822044c5f29
Stas Okhrym <s.okhrym@gmail.com>
2022-09-27 15:34:36 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+class TestCheckPassword:
+    @pytest.mark.parametrize(
+        "password,expected",
+        [
+            ("Pass@word1", True),
+            ("qwerty", False),
+            ("Str@ng", False),
+            ("123_123_asd_asd_asd", False),
+            ("Password12", False),
+            ("_123454321_", False),
+            ("Good_password", False),
+            ("Should_be_@_good_password_mayb3", False),
+            ("Aa1_", False),
+        ]
+    )
+    def test_get_valid_password(
+            self,
+            password,
+            expected
+    ):
+        assert check_password(password) is expected
