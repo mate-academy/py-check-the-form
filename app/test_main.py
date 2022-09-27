ade19136e93dab37c4b1e58624da86ce96731495
Davyd Kryvko <davidkrivko@gmail.com>
2022-09-27 18:55:10 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+from app.main import check_password
+
+
+def test_should_check_long_password():
+    assert not check_password("a@dasdasdaS1dasdasdasda")
+
+
+def test_should_check_short_of_password():
+    assert not check_password("a@dd1A")
+
+
+def test_should_check_up_symbol():
+    assert not check_password("asccsd@1")
+
+
+def test_should_check_special_symbol():
+    assert not check_password("asccsdA1")
+
+
+def test_should_check_number():
+    assert not check_password("asccsd@A")
