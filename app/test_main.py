20b7284baf16288e19be527a82fc221b3aa9eed3
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-09-29 23:27:29 +0300

    Solution for the task

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "given_password,result", [
+        ("PASSWORD", False),
+        ("TOOOOOLONGPASSWOOOORD", False),
+        ("pass", False),
+        ("qwerty", False),
+        ("digitsdigits", False),
+        ("Str@ng", False),
+        ("Pass@word1", True),
+        ("GoodPassword!1", True)
+    ]
+)
+def test_check_password_correct_result(given_password,
+                                       result):
+    assert check_password(given_password) is result
