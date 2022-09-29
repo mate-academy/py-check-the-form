c485cd9b3cfe0ebb1eec53ae786f35a1fb99f8f7
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-09-30 00:49:00 +0300

    Changed code for correct auto tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,20 +1,42 @@
-import pytest
-
 from app.main import check_password
+
+import pytest
 
 
 @pytest.mark.parametrize(
-    "given_password,result", [
-        ("PASSWORD", False),
-        ("TOOOOOLONGPASSWOOOORD", False),
-        ("pass", False),
-        ("qwerty", False),
-        ("digitsdigits", False),
-        ("Str@ng", False),
-        ("Pass@word1", True),
-        ("GoodPassword!1", True)
+    "password,result",
+    [
+        pytest.param(
+            "m1a@tE",
+            False,
+            id="password is too short"
+        ),
+        pytest.param(
+            "Str@ngest",
+            False,
+            id="password has no digits"
+        ),
+        pytest.param(
+            "pass@word1",
+            False,
+            id="password has no upper letters"
+        ),
+        pytest.param(
+            "TOOOOOOL00ONGP@SSWORD",
+            False,
+            id="password is too long"
+        ),
+        pytest.param(
+            "passWORD12",
+            False,
+            id="password has no special symbol"
+        ),
+        pytest.param(
+            "Pass@word1",
+            True,
+            id="correct password"
+        )
     ]
 )
-def test_check_password_correct_result(given_password,
-                                       result):
-    assert check_password(given_password) is result
+def test_check_password(password, result):
+    assert check_password(password) is result
