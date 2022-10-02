7f250c7a9d0fc79914270d1fdfb5d084102cf0a1
AndriyKy <akrechovetskiy@gmail.com>
2022-10-02 17:01:01 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+from app.main import check_password
+
+import pytest
+
+
+@pytest.fixture
+def data_for_test():
+    return {
+        "Pass@word1": True,
+        "Qwertsdf@": False,
+        "pass@word123": False,
+        "P@1": False,
+        "Pass@word1kaslkdfjjlsf": False,
+        "S1trdng": False,
+    }
+
+
+def test_checks_password(data_for_test):
+    for password, boolean in data_for_test.items():
+        assert check_password(password) is boolean
