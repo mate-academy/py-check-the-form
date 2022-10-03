799b2079d9b9663c2469ec18c198805af35f834b
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2022-10-03 21:37:34 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,38 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+class TestCheckPassword:
+    @pytest.mark.parametrize(
+        "password, check",
+        [
+            pytest.param(
+                "",
+                False,
+                id="password is empty string"
+            ),
+            pytest.param(
+                "ABCD123$",
+                True,
+                id="password is valid"
+            ),
+            pytest.param(
+                "abcd123$",
+                False,
+                id="password contains only lower letter"
+            ),
+            pytest.param(
+                "12345678",
+                False,
+                id="password contains only digits"
+            ),
+            pytest.param(
+                "Abcdef0123456789$",
+                False,
+                id="password length more than 16 characters"
+            ),
+        ]
+    )
+    def test_check_password(self, password, check):
+        assert check_password(password) == check
