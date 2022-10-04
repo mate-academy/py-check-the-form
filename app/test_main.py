a53e36cae308ed2fa17b22aaf782d56b4cc0ab70
Oleksandr Murynskyi <a.murinskiy@gmail.com>
2022-10-04 22:06:52 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+class TestCheckPassword:
+    @pytest.mark.parametrize(
+        "password,result",
+        [
+            ("Mate@academy1", True),
+            ("Mateacademy1", False),
+            ("mate", False),
+            ("Mate@academy", False),
+            ("mate@academy1", False),
+            ("111222@222111", False),
+            ("", False),
+            ("Mate@1", False),
+            ("Mate@academyNumber1ever", False)
+
+        ]
+    )
+    def test_valid_password(self, password, result):
+        assert check_password(password) is result
