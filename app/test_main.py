578f35da16dbec6198998d63cf718eb90a9ace0d
Andriy Makarenko <andriy@mate.academy>
2022-10-05 15:28:02 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,42 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        pytest.param(
+            "Fa1$",
+            False,
+            id="Should have at least 8 symbols"
+        ),
+        pytest.param(
+            "Passworabcdefghijklmnopar123@",
+            False,
+            id="Should have no more than 16 symbols"
+        ),
+        pytest.param(
+            "Password@@@",
+            False,
+            id="Should have at least one digit"
+        ),
+        pytest.param(
+            "Password123",
+            False,
+            id="Should have at least one special character"
+        ),
+        pytest.param(
+            "passwordpassword123@",
+            False,
+            id="Should have at least one uppercase letter"
+        ),
+        pytest.param(
+            "Password123@$!",
+            True,
+            id="Should return 'True' if password is valid"
+        )
+    ]
+)
+def test_check_password(password: str, result: bool) -> None:
+    assert check_password(password) == result
