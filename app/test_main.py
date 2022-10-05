234399979767983540b193ef3f1dea5e1f79a8f6
Dmytro Skrypka <2346677tyu@gmail.com>
2022-10-05 21:17:32 +0300

    Solution new parameter

--- app/test_main.py
+++ app/test_main.py
@@ -3,31 +3,37 @@ import pytest
 from app.main import check_password
 
 
-@pytest.mark.parametrize(
-    "password, expected",
-    [
-        pytest.param(
-            "Pass@word1", True,
-            id="Should return 'True'"
-        ),
-        pytest.param(
-            "qwerty", False,
-            id="Should return 'False'"
-        ),
-        pytest.param(
-            "Str@ng", False,
-            id="Should return 'False'"
-        ),
-        pytest.param(
-            "12345", False,
-            id="Should return 'False'"
-        ),
-        pytest.param(
-            "H33l0 w0rld", False,
-            id="Should return 'False'"
-        )
-    ]
-)
-def test_check_password(password: str,
-                        expected: bool) -> None:
-    assert check_password(password) == expected
+class TestCheckPassword:
+    @pytest.mark.parametrize(
+        "password, expected",
+        [
+            pytest.param(
+                "Pass@word1", True,
+                id="Should return 'True' "
+            ),
+            pytest.param(
+                "qwerty", False,
+                id="Should return 'False'"
+            ),
+            pytest.param(
+                "Str@ng", False,
+                id="Should return 'False'"
+            ),
+            pytest.param(
+                "12345", False,
+                id="Should return 'False'"
+            ),
+            pytest.param(
+                "H33l0 w0rld", False,
+                id="Should return 'False'"
+            ),
+            pytest.param(
+                "Pass@word1Pass@word1Pass@word1", False,
+                id="Should return 'False'"
+            )
+        ]
+    )
+    def test_check_password(self,
+                            password: str,
+                            expected: bool) -> None:
+        assert check_password(password) == expected
