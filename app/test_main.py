6e098847e54750b40ed8cdba296a7a37cbc223df
Serhii Kovalenko <27kovalenko@gmail.com>
2022-10-06 12:33:06 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+from app.main import check_password
+
+
+def test_should_return_false_for_password_without_digits() -> bool:
+    assert check_password("P@ssword") is False
+
+
+def test_should_return_false_for_too_long_password() -> bool:
+    assert check_password("P@ssword1234567890") is False
+
+
+def test_should_return_false_for_too_short_password() -> bool:
+    assert check_password("Qwe1@") is False
+
+
+def test_should_return_false_for_password_without_special_symbols() -> bool:
+    assert check_password("Password1") is False
+
+
+def test_should_return_false_for_password_without_uppercase_latter() -> bool:
+    assert check_password("p@ssword1") is False
