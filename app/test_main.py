72a9d2d3619bec3ab1dadc9c183b5a8846ffcf12
Dmytro Skrypka <2346677tyu@gmail.com>
2022-10-06 13:57:08 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -3,45 +3,43 @@ import pytest
 from app.main import check_password
 
 
-class TestCheckPassword:
-    @pytest.mark.parametrize(
-        "password, expected",
-        [
-            pytest.param(
-                "Pass@word1", True,
-                id="Should return 'True' "
-            ),
-            pytest.param(
-                "Pass@word1Pass@word1Pass@word1", False,
-                id="Should return returns False for"
-                   " too long passwords"
-            ),
-            pytest.param(
-                "Str@ng", False,
-                id="Should return 'False'"
-            ),
-            pytest.param(
-                "Password1", False,
-                id="Should returns False for passwords"
-                   " without special symbols"
-            ),
-            pytest.param(
-                "Password", False,
-                id="Should returns False for passwords"
-                   " without digits"
-            ),
-            pytest.param(
-                "abrakadabra", False,
-                id="Should  returns False for passwords"
-                   " without uppercase letter"
-            ),
-            pytest.param(
-                "Pass", False,
-                id="Should returns False for short passwords"
-            )
-        ]
-    )
-    def test_check_password(self,
-                            password: str,
-                            expected: bool) -> None:
-        assert check_password(password) == expected
+@pytest.mark.parametrize(
+    "password, expected",
+    [
+        pytest.param(
+            "Pass@word1", True,
+            id="Should return 'True' "
+        ),
+        pytest.param(
+            "Pass@word1Pass@word1Pass@word1", False,
+            id="Should return returns False for"
+               " too long passwords"
+        ),
+        pytest.param(
+            "Str@nglkjhlkh", False,
+            id="Should return 'False'"
+        ),
+        pytest.param(
+            "Password1", False,
+            id="Should returns False for passwords"
+               " without special symbols"
+        ),
+        pytest.param(
+            "Password", False,
+            id="Should returns False for passwords"
+               " without digits"
+        ),
+        pytest.param(
+            "password1234@", False,
+            id="Should  returns False for passwords"
+               " without uppercase letter"
+        ),
+        pytest.param(
+            "Pass12@", False,
+            id="Should returns False for short passwords"
+        )
+    ]
+)
+def test_check_password(password: str,
+                        expected: bool) -> None:
+    assert check_password(password) == expected
