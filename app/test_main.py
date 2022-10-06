a66237e21fddc7a7ccbb8906f4df8e3b5971ed61
Edik Marushchak <e.moscow138@gmail.com>
2022-10-06 11:22:07 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -2,19 +2,19 @@ from app.main import check_password
 
 
 def test_strong_password() -> None:
-    assert check_password("p_ASS#wrd4") is True
+    assert check_password("Pass@word1") is True
 
 
 def test_short_password() -> None:
-    assert check_password("01@ss") is False
+    assert check_password("k01A$$") is False
 
 
 def test_long_password() -> None:
-    assert check_password("23456987655493457395395739485") is False
+    assert check_password("Parol#7655493457395395785") is False
 
 
 def test_password_without_numbers() -> None:
-    assert check_password("Edikedik") is False
+    assert check_password("Edikedik_$") is False
 
 
 def test_password_without_upper() -> None:
