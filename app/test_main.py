f88c47cfdf2cde41518819313a65a32d6c40a1f6
Edik Marushchak <e.moscow138@gmail.com>
2022-10-06 11:16:22 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -2,7 +2,7 @@ from app.main import check_password
 
 
 def test_strong_password() -> None:
-    assert check_password("pASS%$@wrd4") is True
+    assert check_password("p_ASS#wrd4") is True
 
 
 def test_short_password() -> None:
