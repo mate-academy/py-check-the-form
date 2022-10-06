6ee4dac54c11c2d4cd897f5a583e3911e4c7d12c
Alexander <alexzubkoff123@gmail.com>
2022-10-06 17:15:10 +0300

    Test for pass validation

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,35 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected",
+    [
+        (
+            "Pass@word1",
+            True,
+        ),
+        (
+            "Pass@word",
+            False,
+        ),
+        (
+            "Pass@worddfgsdfgdfgsafdsdf1",
+            False,
+        ),
+        (
+            "Pas@wf1",
+            False,
+        ),
+        (
+            "pass@word1",
+            False,
+        ),
+    ]
+)
+def test_can_access_google_page(
+        password: str,
+        expected: bool
+) -> None:
+
+    assert check_password(password) == expected
