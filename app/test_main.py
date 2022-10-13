b04d26ee2d5adcd556b9c31dfb0a4476f8c2e25f
Dmytro Kyrpan <d.kirpan@gmail.com>
2022-10-13 12:31:42 +0300

    solution v01

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+from app.main import check_password
+
+
+def test_contains_letters_and_digits_and_special_character() -> None:
+    assert check_password("Password123") is False
+    assert check_password("P!ssword123") is True
+    assert check_password("P@ssword") is False
+
+
+def test_too_long_password() -> None:
+    assert check_password("P@ssword123P@ssword123P@ssword123") is False
+    assert check_password("P@ssword123P@ss") is True
+
+
+def test_too_short_password() -> None:
+    assert check_password("P@ss123") is False
+    assert check_password("P@ssword123Pss!1") is True
