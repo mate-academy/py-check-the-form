bf62cc9e6ab11755488e84869741aa1ac7050a62
Misha Doroshenko <doroshenko.misha73@gmail.com>
2022-10-16 12:29:55 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,22 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected",
+    [
+        pytest.param("Pass$@#&!-_34", True),
+        pytest.param("$@#3&!PASS-_4", True),
+        pytest.param("Pass$@#&!34+", False),
+        pytest.param("pass$@#&!-_34", False),
+        pytest.param("Password@#", False),
+        pytest.param("Password234", False),
+        pytest.param("Pass$84", False),
+        pytest.param("Pass$_84", True),
+        pytest.param("Pass$_8412344321!", False),
+        pytest.param("", False)
+    ]
+)
+def test_check_password(password: str, expected: bool) -> None:
+    assert check_password(password) is expected
