653ffd30c6b2158a72a16c723855cc0f6fcb222c
Andrey Losev <andry07070712@gmail.comless.gitconfiggit config --global user.email andry07070712@gmail.com>
2022-10-28 18:17:49 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+from app.main import check_password
+
+
+def test_check_password_return_false_for_short_password():
+    assert check_password("@mC1") is False
+
+
+def test_check_password_return_false_for_long_password():
+    assert check_password("@@@HytgHukiy23tt@@@@gfgfggv") is False
+
+
+def test_check_password_return_false_for_password_no_symbols():
+    assert check_password("Holah0ype") is False
+
+
+def test_check_password_return_false_for_password_no_big_letter():
+    assert check_password("m@cb00kair") is False
+
+
+def test_check_password_return_false_for_password_no_digits():
+    assert check_password("Holah@ype") is False
