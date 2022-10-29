78584f3dd8b23b8aa5b3098684972c37c03d5e1e
George <georgepavlej@gmail.com>
2022-10-29 19:47:33 +0100

    Solution to check password

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,46 @@
-# write your code here
+from app.main import check_password
+import pytest
+
+
+class TestCheckPassword:
+    @pytest.mark.parametrize(
+        "expected_password,expected_result",
+        [
+            pytest.param(
+                "Password1",
+                False,
+                id="passwords needs to be without without special symbols"
+            ),
+            pytest.param(
+                "Pass@w1",
+                False,
+                id="password is too short"
+            ),
+            pytest.param(
+                "Pass@word",
+                False,
+                id="password needs to be without digits"
+            ),
+            pytest.param(
+                "Pass@wordPass@word1",
+                False,
+                id="the password is too long"
+            ),
+            pytest.param(
+                "Pass@word1",
+                True,
+                id="all checks are done correctly"
+            ),
+            pytest.param(
+                "pass@word1",
+                False,
+                id="password needs to be without upper letters"
+            )
+        ]
+    )
+    def test_check_password(
+            self,
+            expected_password: str,
+            expected_result: bool
+    ) -> None:
+        assert check_password(expected_password) == expected_result
