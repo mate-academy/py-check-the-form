5d42ba61021e2e95c29726d577cf8fe2970d7563
Mykola Zakharov <kolyazakharov94@gmail.com>
2022-11-03 15:26:08 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+from app.main import check_password
+
+
+def test_should_return_false_when_password_less_than_8_symbols() -> None:
+    assert check_password("Qwer1@") is False
+
+
+def test_should_return_false_when_password_more_than_16_symbols() -> None:
+    assert check_password("Qwertyuiopasdfghjklzxcvb1@") is False
+
+
+def test_should_return_false_when_all_symbols_are_lower() -> None:
+    assert check_password("qwertyu1@") is False
+
+
+def test_should_return_false_when_there_is_no_digit_in_password() -> None:
+    assert check_password("Qwertyui@") is False
+
+
+def test_should_return_false_when_password_without_special_symbols() -> None:
+    assert check_password("qwerty1234") is False
+
+
+def test_should_return_true_when_password_is_valid() -> None:
+    assert check_password("Qwerty1@34") is True
