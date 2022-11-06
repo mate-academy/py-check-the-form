68c3610b68ca320e74d43953922c86567022fc9b
Dede <specialized8393@gmail.com>
2022-11-06 11:16:09 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,22 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "pwd,result",
+    [
+        pytest.param("Str@ng1", False,
+                     id="short passwords"),
+        pytest.param("Pass@word13A!asd1", False,
+                     id="too long passwords"),
+        pytest.param("pass@word13", False,
+                     id="passwords without uppercase letter"),
+        pytest.param("Qwerty!A", False,
+                     id="passwords without digits"),
+        pytest.param("Password1", False,
+                     id="passwords without special symbols"),
+    ]
+)
+def test_func_check_password_with_param(pwd: str, result: bool) -> None:
+    assert check_password(pwd) is result
