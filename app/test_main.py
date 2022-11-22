4048b79baa1b8971a0b4866737d41d2d63eb4b6c
Ihor Ivashov <dm-i@i.ua>
2022-11-22 15:26:06 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+from app.main import check_password
+
+
+def test_too_short_password() -> None:
+    assert check_password("Ab1@f") is False
+
+
+def test_too_long_password() -> None:
+    assert check_password("aAbcdfghklj1@ljmlwswervmllknlknasdvkn") is False
+
+
+def test_upper_case() -> None:
+    assert check_password("abcdf17@dsfff") is False
+
+
+def test_digits() -> None:
+    assert check_password("Fbcdf@dsfff") is False
+
+
+def test_special() -> None:
+    assert check_password("Fbcdfds1fff") is False
+
+
+def test_correct_password() -> None:
+    assert check_password("Fbcdf1567@dsfff") is True
