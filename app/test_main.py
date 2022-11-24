bbe54886729f515cbaf78d1f42da4e12c2de3740
vani@linux <winfkill@gmail.com>
2022-11-24 15:16:58 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,13 +6,13 @@ class TestCheckPassValidClass:
     @pytest.mark.parametrize(
         "password,result",
         [
-            ("Pass@word1",True),
-            ("qwerty",False),
-            ("Str@ng",False),
-            ("symbolisation",False),
-            ("ewqwjkenqwjenqnwekjqnwkejnqwekjqwnej",False),
-            ("Pass@",False)
-        ]
+            ("Pass@word1", True),
+            ("qwerty", False),
+            ("Str@ng", False),
+            ("symbolisation", False),
+            ("ewqwjkenqwjenqnwekjqnwkejnqwekjqwnej", False),
+            ("Pass@", False),
+        ],
     )
-    def test_check_password_is_valid(self, password, result):
+    def test_check_password_is_valid(self, password: str, result: bool) -> None:
         assert check_password(password) == result
