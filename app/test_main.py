30c244cbdb30d34ba6ca8ca981576c2a66485d8b
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2022-11-29 16:59:57 +0200

    password cheker

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+from app.main import check_password
+
+
+def test_amount_characters_more_then_sixteen() -> None:
+    assert check_password("Pass@word1assdfggd") is False
+
+
+def test_password_without_digits() -> None:
+    assert check_password("Pass@worda") is False
+
+
+def test_password_without_uppercase() -> None:
+    assert check_password("pas@sword1") is False
+
+
+def test_password_short_password() -> None:
+    assert check_password("Pa@1") is False
