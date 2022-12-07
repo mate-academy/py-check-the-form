606534ca546bac5efb4372ee6a8e6efce1c3b0b0
soobig666 <darkvader.ukr.net@gmail.com>
2022-12-07 23:32:51 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,38 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "actual,excepted",
+    [
+        pytest.param(
+            "qwerty1234@",
+            False,
+            id="password should has 1 uppercase letter"
+        ),
+        pytest.param(
+            "Qwerty1234",
+            False,
+            id="password should has 1 special character"
+        ),
+        pytest.param(
+            "Qwerty@qwe",
+            False,
+            id="password should has 1 digit"
+        ),
+        pytest.param(
+            "Pass@word11111111",
+            False,
+            id="password should have maximum 16 characters inclusive"
+        ),
+        pytest.param(
+            "Pass@w1",
+            False,
+            id="password should have least 8 characters"
+        )
+    ]
+)
+def test_without_upper_letter(
+        actual: str,
+        excepted: bool) -> None:
+    assert check_password(actual) == excepted
