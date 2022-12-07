96db13ceabc5d7a2c12adcf5d6bf766811f292c5
Maksym Maksimov <maksymmaksimov88@gmail.com>
2022-12-07 11:37:09 +0800

    Solution Maksym Maksimov

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+from app.main import check_password
+
+
+def test_password_not_match_min_length() -> None:
+    assert check_password("Mima8@") is False
+
+
+def test_password_not_match_max_length() -> None:
+    assert check_password("Mima_hen_chang_1@1") is False
+
+
+def test_password_has_no_uppercase_letter() -> None:
+    assert check_password("pass@word1") is False
+
+
+def test_password_has_no_digit() -> None:
+    assert check_password("Pass@word") is False
+
+
+def test_password_has_no_special_character() -> None:
+    assert check_password("Password1") is False
+
+
+def test_valid_password_meets_all_rules() -> None:
+    assert check_password("Pass@word1") is True
