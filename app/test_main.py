57a32b493033cab0ca868c7b64cc1fd97c62b9bf
Daria Hannochenko <soloha7965@gmail.com>
2022-12-07 19:04:03 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,47 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+class TestCheckPassword:
+    @pytest.mark.parametrize(
+        "initial_password,expected_result",
+        [
+            pytest.param(
+                "Pass@word1",
+                True,
+                id="should check upper, special, digit"
+            ),
+
+            pytest.param(
+                "qwerty1@",
+                False,
+                id="should check upper"
+            ),
+
+            pytest.param(
+                "Str@ng",
+                False,
+                id="should check digit and length"
+            ),
+
+            pytest.param(
+                "ddlgkk1@gllgllglglgllglglgAg",
+                False,
+                id="should check maximum length"
+            ),
+
+            pytest.param(
+                "Ddf1@",
+                False,
+                id="should check minimum length"
+            )
+
+        ]
+    )
+    def test_understatnd_isogram_correctly(
+            self,
+            initial_password: str,
+            expected_result: bool
+    ) -> None:
+        assert check_password(initial_password) == expected_result
