eb04ff26a5108f5c3c641239524889530ac44798
Vitalii Panochko <vpapplic@gmail.com>
2023-01-04 16:07:52 +0200

    tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,41 @@
-# write your code here
+from app.main import check_password
+import pytest
+
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        pytest.param(
+            "qwewqeewqewq",
+            False,
+            id="test_should_check_upper_letter"
+        ),
+        pytest.param(
+            "qweqweqwe",
+            False,
+            id="test_should_check_digit"
+        ),
+        pytest.param(
+            "qweqweqwe",
+            False,
+            id="test_should_check_special_symbols"
+        ),
+        pytest.param(
+            "qweqwe",
+            False,
+            id="test_should_check_min_length"
+        ),
+        pytest.param(
+            "qweqweqweqweqweqwe",
+            False,
+            id="test_should_check_max_length"
+        ),
+        pytest.param(
+            "Pass@word1",
+            True,
+            id="1 digit, 1 special character, 1 uppercase letter."
+        )
+    ]
+)
+def test_password(password: str, result: bool) -> None:
+    assert check_password(password) is result
