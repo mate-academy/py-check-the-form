816c852b370ea1763f4967f810ca5e551ed7f91b
Artur Leleiko <artur_98@ukr.net>
2023-01-15 00:13:57 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        ("Pass@word1", True),
+        ("qwerty", False),
+        ("Str@ng", False),
+        ("justlower4case", False),
+        ("$mAl4", False),
+        ("vEryLongPasswOrd444matEbig$", False),
+        ("pythonTop1", False),
+        ("Academy@&", False),
+        ("england&789", False)
+    ]
+)
+def test_should_return_correct_answer(password: str, result: bool) -> None:
+    assert check_password(password) == result
