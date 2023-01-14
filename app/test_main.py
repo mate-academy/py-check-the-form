9c20c5dcc3eb432424c9aff3d335aaccb43b6661
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-01-14 10:24:17 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,19 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password,response",
+    [
+        ("Str@ng1", False),
+        ("only1owerc@se", False),
+        ("Extreme11yLongP@sword", False),
+        ("Forbidden%^)", False),
+        ("WithoutDigit", False),
+        ("W1thoutSpecChar", False),
+        ("Pass@word1", True)
+    ]
+)
+def test_cases_of_password(password: str, response: bool) -> None:
+    assert (check_password(password) == response)
