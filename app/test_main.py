4e6b343114deff4706eeb8e21a2b90c1c6e2c45d
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-16 12:32:09 +0200

    1 commit

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,24 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        ("aA12@", False),
+        ("$@#&!-_", False),
+        ("$@#&!-jdjkrjr", False),
+        ("$@#1A&!-_", True),
+        ("Pass@word1", True),
+        ("qwerty", False),
+        ("Str@ng", False),
+        ("Str@HH1HHHGGGGGGG", False),
+        ("Aa$GFHFHFFH", False),
+        ("a1GGDGGDGH", False),
+        ("pass@word1", False),
+    ]
+
+)
+def test_check_password(password: str, result: bool) -> None:
+    assert check_password(password) == result
