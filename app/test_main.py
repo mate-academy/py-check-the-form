9b55db4046c35863d2310deebdb871784cbfcf16
Andriy <andriykrav@gmail.com>
2023-02-18 13:24:37 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -8,11 +8,13 @@ from .main import check_password
         ("Pass@word1", True),
         ("qwerty", False),
         ("Str@ng", False),
-        ("Str@ngdcvwvvgsegwegcsadcsac", False),
+        ("S1tr@ngdcvwvvgsegwegcsadcsac", False),
         ("123456df", False),
         ("A2@e", False),
-        ("A2@487979ghjgkj", False),
+        ("A2@48er", False),
         ("q@1rtyfg", False),
+        ("Ra1rtyfg", False),
+        ("Ra@rtyfg", False),
     ]
 )
 def test_check_password(password: str, result: bool) -> None:
