cb06b3489c2d20d24e210d504b21e9fa53473d05
Andriy <andriykrav@gmail.com>
2023-02-18 13:16:13 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -3,30 +3,18 @@ from .main import check_password
 
 
 @pytest.mark.parametrize(
-    "has_upper, has_digit, has_special,pass_len, password",
+    "password, result",
     [
-        (True, True, True, True, "Pass@word1"),
-        (False, True, True, True, "q@1rtyfg"),
-        (True, False, True, False, "Str@ng"),
-        (True, False, True, False, "Str@ngsdcjsc"),
-        (False, True, False, True, "123456df"),
-        (False, True, False, False, "12345df"),
-        (False, True, True, False, "12345df1234rfwf@"),
-        (True, True, True, False, "A2@e"),
-        (True, True, True, False, "A2@487979ghjgkjg"),
+        ("Pass@word1", True),
+        ("qwerty", False),
+        ("Str@ng", False),
+        ("Str@ngdcvwvvgsegwegcsadcsac", False),
+        ("123456df", False),
+        ("A2@e", False),
+        ("A2@487979ghjgkj", False),
+        ("q@1rtyfg", False),
     ]
 )
-def test_check_password(
-    has_upper: bool,
-    has_digit: bool,
-    has_special: bool,
-    pass_len: bool,
-    password: str
-) -> None:
-    password_check = check_password("Pass@word1")
-    passw = bool
-    if len(password) is True:
-        if passw == all([has_upper, has_digit, has_special]):
-            assert password_check == passw
-        elif passw == any([has_upper, has_digit, has_special, pass_len]):
-            assert password_check == passw
+def test_check_password(password, result):
+    password_check = check_password(password)
+    assert password_check == result
