287541d56e99210f3cfe20f40e2244924dc5ec11
Maksym Nikityuk <maksym71417@gmail.com>
2023-02-21 21:57:23 +0200

    First Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+from app.main import check_password
+
+
+def test_length_lower() -> None:
+    assert check_password("J56&") is False
+
+
+def test_length_upper() -> None:
+    assert check_password("J56&hdajhasdhgjahsgdjh") is False
+
+
+def test_has_upper() -> None:
+    assert check_password("t40@rere") is False
+
+
+def test_has_digit() -> None:
+    assert check_password("Yt$UiUiU") is False
+
+
+def test_has_special() -> None:
+    assert check_password("567YtuYu") is False
+
+
+def test_has_all_requirements() -> None:
+    assert check_password("$HeyYou56") is True
