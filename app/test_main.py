e4bf562c9f0336b702052045c166e082cc4d0338
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-02-22 19:09:29 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+def test_valid_password():
+    assert check_password("Abc123$#") is True
+
+
+def test_invalid_password_length():
+    assert check_password("aBc1$") is False
+
+
+def test_invalid_password_no_uppercase():
+    assert check_password("abc123$#") is False
+
+
+def test_invalid_password_no_digit():
+    assert check_password("Abcdefgh$#") is False
+
+
+def test_invalid_password_no_special_character():
+    assert check_password("Abc1234567") is False
+
+
+def test_invalid_password_false_when_pass_length_more_16() -> None:
+    assert check_password("Password_BiggerThan16") is False
