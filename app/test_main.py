5a5fe8ed45c131629b579aa3c21e5755617f62d3
Eugene <eugene.lot77@gmail.com>
2023-02-23 16:09:45 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,47 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password,access",
+    [
+        pytest.param(
+            "Password@1",
+            True,
+            id="test correct password length"
+        ),
+        pytest.param(
+            "Pass@1",
+            False,
+            id="test password length less then 8"
+        ),
+        pytest.param(
+            "PasswordPasswordPassword@1",
+            False,
+            id="test password length greater then 16"
+        ),
+        pytest.param(
+            "12345678@",
+            False,
+            id="test password without any letter"
+        ),
+        pytest.param(
+            "password1@",
+            False,
+            id="test password without any upper letter"
+        ),
+        pytest.param(
+            "Password@",
+            False,
+            id="test password without any digit"
+        ),
+        pytest.param(
+            "Password1",
+            False,
+            id="test password without any special symbol"
+        )
+    ]
+)
+def test_check_password(password: str, access: bool) -> None:
+    assert check_password(password) is access
