cccb5eff2631ab48ee34f617090da43a7e330cbe
Mykhailo Benzel <mishabenzel1997@gmail.com>
2023-03-02 22:12:51 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+from app.main import check_password
+
+
+def test_should_return_false_for_short_passwords() -> None:
+    assert check_password("1@weW") is False
+
+
+def test_should_return_false_for_long_passwords() -> None:
+    assert check_password("qwerty12@Wfewfgrgdffds") is False
+
+
+def test_should_return_false_for_password_without_digits() -> None:
+    assert check_password("Qwerty@#QW") is False
+
+
+def test_should_return_false_for_password_without_uppercase() -> None:
+    assert check_password("qwe123@$qwe") is False
+
+
+def test_should_return_false_for_password_without_spec_char() -> None:
+    assert check_password("qwer123QWE") is False
