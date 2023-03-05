00ec423ae955f28ab1dabe45856c40f0bdfdaa39
Yulia <Mala>
2023-03-05 20:49:36 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,22 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password,result",
+    [
+        pytest.param("Aqwer12!*", False, id="unsupported special character"),
+        pytest.param("Aqwer12!И", False, id="not latin letter"),
+        pytest.param("Qwerty9&", True, id="8 symbols"),
+        pytest.param("Asdfghjkl9876$_-", True, id="16 symbols"),
+        pytest.param("As876$_", False, id="7 symbols"),
+        pytest.param("Asdfghjkl90876$_-", False, id="17 symbols"),
+        pytest.param("Lkjhhgf$_", False, id="no digit"),
+        pytest.param("kkks876$_", False, id="no uppercase"),
+        pytest.param("As876jsla", False, id="no special character"),
+
+    ]
+)
+def test_check_password(password: str, result: bool) -> None:
+    assert check_password(password) == result
