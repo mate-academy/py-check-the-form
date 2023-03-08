7cf46a33492b74a2b9d3a712ef3a1da2c2bf5393
ZaikaDaria <corplinebusiness@gmail.com>
2023-03-08 22:50:46 +0200

    Add solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        ("Amwee11", False),
+        ("Qhj3rty9&3", True),
+        ("Ajdnwhjkl90876$!-", False),
+        ("kda^W)0", False),
+        ("Hkhlhhgf*_", False),
+        ("wkkdks876$_", False),
+    ],
+    ids=[
+        "Special character missing",
+        "Password length 8-16 symbols",
+        "Password length more then 16 symbols",
+        "Password length less then 8 symbols",
+        "Digits missing",
+        "Uppercase letters missing",
+
+    ]
+)
+def test_check_password(password: str, result: bool) -> None:
+    assert check_password(password) == result
