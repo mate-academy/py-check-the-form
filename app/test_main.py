028fa7dfa044e1efd392cb8e5938372adab6fd61
Anastasiia <nastyashch1601@gmail.com>
2023-03-17 21:05:01 +0000

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,19 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        pytest.param("Qwer1!", False, id="password is too short"),
+        pytest.param("Qwertytuiopasgdjfk1!", False, id="password is too long"),
+        pytest.param("Qwertyui!@", False, id="no digits"),
+        pytest.param("Qwertyu123", False, id="no special character"),
+        pytest.param("q12345678!@", False, id="no upper letter"),
+    ]
+)
+def test_password_validity(
+    password: str,
+    result: bool
+) -> None:
+    assert (check_password(password) == result)
