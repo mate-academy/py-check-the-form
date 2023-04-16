b93c79b763f1a61c2c6b5939b7c96acf0f033bec
Mykola <nickkozlov90@gmail.com>
2023-04-16 22:57:11 +0300

    solution3

--- app/test_main.py
+++ app/test_main.py
@@ -6,13 +6,11 @@ from app.main import check_password
     "string,result",
     [
         ("Pass@word1", True),
-        ("qwerty", False),
-        ("Str@ng", False),
+        ("P@ert1", False),
+        ("pass@word1", False),
         ("Pass@word1Pass@word1", False),
-        ("Pass%wrd1", False),
-        ("Pass*word1", False),
-        ("Pass(@)word1", False),
-        ("Pass+word1", False)
+        ("Password1", False),
+        ("Pass@word", False)
     ]
 )
 def test_check_password_is_valid(string: str, result: bool) -> None:
