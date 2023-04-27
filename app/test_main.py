db6fcc8bdf64d98698ec2adbf667e3827d7d1fa1
Maksym Bezhevets <begik4@gmail.com>
2023-04-27 17:14:03 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,27 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        pytest.param("Pass@word1", True,
+                     id="Test correct password"),
+        pytest.param("qwerty", False,
+                     id="Test qwerty password"),
+        pytest.param("Str@ng", False,
+                     id="Test short password length when other rules correct"),
+        pytest.param("Pass@word1Str@ng123", False,
+                     id="Test long password length when other rules correct"),
+        pytest.param("П@роль123", False,
+                     id="Test if text in Cyrillic"),
+        pytest.param("p@@s$@#&!1", False,
+                     id="Test if many  special characters"),
+    ]
+)
+def test_check_password(
+        password: str,
+        result: bool
+) -> None:
+    assert check_password(password) == result
