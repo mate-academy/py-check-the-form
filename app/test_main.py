cc42309be6c96ca0b366fa8e37b611988c619bd0
Munkoor <eee1lvlboss@gmail.com>
2023-05-17 17:29:47 +0300

    fixed a code

--- app/test_main.py
+++ app/test_main.py
@@ -6,13 +6,15 @@ from app.main import check_password
 @pytest.mark.parametrize(
     "password, result",
     [
-        pytest.param("qwerty", False),
-        pytest.param("qwertyuiopasdfghj", False),
-        pytest.param("слава Україні", False),
-        pytest.param("Geroyamslava@", False),
-        pytest.param("S1mpleIsGO@T", True),
+        pytest.param("qWe@ty1", False, id="password short, need more symbols"),
+        pytest.param("qwertyuiopasdfghj", False,
+                     id="Password too long should be less symbols"),
+        pytest.param("славаУкраїні@1", False,
+                     id="Password should contain only latin lettes"),
+        pytest.param("Geroyamslava1", False,
+                     id="Password should contain at least 1 special symbol"),
         pytest.param("Pass@word1", True)
     ]
 )
 def test_check_password(password: str, result: bool) -> None:
-    assert check_password(password) == result
+    assert check_password(password) is result
