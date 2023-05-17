ea6800c093e2f238e6e98700dd46e44bda4f0a32
Ivan <maramorosh2@gmail.com>
2023-05-17 16:03:43 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+from app.main import check_password
+
+
+def test_check_password_lower_then_eight_characters():
+    assert check_password("Qwert-1") is False
+
+def test_check_password_higher_then_sixteen_characters():
+    assert check_password("Qwertyuiopasdfghj-1") is False
+
+def test_no_uppercase_character():
+    assert check_password("qwerty-1") is False
+
+def test_no_digit():
+    assert check_password("Qwerty-i") is False
+
+def test_no_special_character():
+    assert check_password("Qwertyu1") is False
+
+def test_correct_password():
+    assert check_password("Qwerty-1") is True
