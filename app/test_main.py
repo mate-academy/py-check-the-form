3f67324c78b8b0d84bcfde4dbe57233d2dba1464
Munkoor <eee1lvlboss@gmail.com>
2023-05-17 16:54:36 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password,result",
+    [
+        pytest.param("qwerty", False),
+        pytest.param("qwertyuiopasdfghj", False),
+        pytest.param("слава Україні", False),
+        pytest.param("Geroyamslava@", False),
+        pytest.param("S1mpleIsGO@T", True)
+    ]
+)
+def test_check_correct_password(password: str, result: bool) -> None:
+    assert check_password(password) == result
