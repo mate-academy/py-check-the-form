8887b7927b8e43750049e65a832be4e056af248f
Oleksandr Gubarev <alexgubarev885@gmail.com>
2023-05-22 14:13:30 +0300

    Py check password

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+from app.main import check_password
+
+
+def test_check_max_len() -> None:
+    assert check_password("adbdhdjskdjadjadjjcaas") is False
+
+
+def test_check_min_len() -> None:
+    assert check_password("adbdh") is False
+
+
+def test_upper_case() -> None:
+    assert check_password("Adbdh") is False
+
+
+def test_right() -> None:
+    assert check_password("Pass@word1") is True
+
+
+def test_not_digits() -> None:
+    assert check_password("Pass@word") is False
+
+
+def test_not_special_symbols() -> None:
+    assert check_password("Password1") is False
