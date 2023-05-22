290de25818f85347752874591a9ff091d1da92ae
Oleksandr Gubarev <alexgubarev885@gmail.com>
2023-05-22 14:18:41 +0300

    Fix Py check password

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,45 @@
+import pytest
+
 from app.main import check_password
 
 
-def test_check_max_len() -> None:
-    assert check_password("adbdhdjskdjadjadjjcaas") is False
-
-
-def test_check_min_len() -> None:
-    assert check_password("adbdh") is False
-
-
-def test_upper_case() -> None:
-    assert check_password("Adbdh") is False
-
-
-def test_right() -> None:
-    assert check_password("Pass@word1") is True
-
-
-def test_not_digits() -> None:
-    assert check_password("Pass@word") is False
-
-
-def test_not_special_symbols() -> None:
-    assert check_password("Password1") is False
+@pytest.mark.parametrize(
+    "password_to_check, expected_answer",
+    [
+        pytest.param(
+            "Abc!1234",
+            True,
+            id="Should return True"
+        ),
+        pytest.param(
+            "Abc!123",
+            False,
+            id="Should return that password is too short"
+        ),
+        pytest.param(
+            "Abc!123123123123321123",
+            False,
+            id="Should return that password is too long"
+        ),
+        pytest.param(
+            "abc!1234",
+            False,
+            id="Should return that password has no uppercase letter"
+        ),
+        pytest.param(
+            "Abc!abcd",
+            False,
+            id="Should return that password has no digits"
+        ),
+        pytest.param(
+            "Abc12345",
+            False,
+            id="Should return that password has no special character"
+        )
+    ]
+)
+def test_password_checking(
+        password_to_check: str,
+        expected_answer: bool
+) -> None:
+    assert check_password(password_to_check) == expected_answer
