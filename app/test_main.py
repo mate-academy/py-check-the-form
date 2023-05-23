05e75fe32a140193b17e0d0a2a5bc0f414c87c5c
Bohdan11Dii <bodyabrovdiy@gmail.com>
2023-05-23 14:39:21 +0300

    Create test for check password

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,19 @@
 # write your code here
+from app.main import check_password
+
+
+def test_without_upper() -> None:
+    assert check_password("world@wordh1") is False
+
+
+def test_password_without_digit() -> None:
+    assert check_password("Pass@word") is False
+
+
+def test_should_be_min_and_max_len() -> None:
+    assert check_password("Tre1@") is False
+    assert check_password("Rbhab3b8!8@knaeini@") is False
+
+
+def test_should_contain_all_character() -> None:
+    assert check_password("Pass@wld1we") is True
