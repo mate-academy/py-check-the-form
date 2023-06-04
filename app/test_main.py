c32766428773f5a945d3304b3c10b37cba19910f
yuro <yuro740@gmail.com>
2023-06-04 19:18:54 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -7,7 +7,7 @@ from app.main import check_password
     "password,result",
     [
         ("SuperP@ss007", True),
-        ("SuperPuperVeryP@ss007", False),
+        ("SuperHyperVeryP@ss007", False),
         ("P@ss007", False),
         ("P@ss()()/", False),
         ("superp@ss007", False),
