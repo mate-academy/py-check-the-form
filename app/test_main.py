2875d1203a7b3ca7f6c91b16d43aec53c28815d0
yuro <yuro740@gmail.com>
2023-06-04 19:17:09 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,19 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password,result",
+    [
+        ("SuperP@ss007", True),
+        ("SuperPuperVeryP@ss007", False),
+        ("P@ss007", False),
+        ("P@ss()()/", False),
+        ("superp@ss007", False),
+        ("Superp@ss", False),
+        ("SuperPass007", False),
+    ]
+)
+def test_check_password(password: str, result: bool) -> None:
+    assert check_password(password) == result
