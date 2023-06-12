9fbd347c3e550d5e8c1800a56c59652fd4ef500e
Nikita <melniknikita2002@icloud.com>
2023-06-12 22:05:33 +0300

    task

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+from app.main import check_password
+
+import pytest
+
+
+class TestCheckPassword:
+
+    @pytest.mark.parametrize(
+        "password,result",
+        [
+            ("P@ss1wd", False),
+            ("Pass@word1passwor", False),
+            ("Pass@word", False),
+            ("Password1", False),
+            ("pass#word2", False)
+        ]
+    )
+    def test_check_password_is_correct(
+            self, password: str,
+            result: bool
+    ) -> None:
+
+        assert check_password(password) == result
