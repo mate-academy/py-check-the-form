a1ee852ca0ed2d6c03e640afb651acc1bd53ef44
Jorjio <Jorjio22@gmail.com>
2023-06-12 19:58:38 +0000

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,8 @@
-# write your code here
+from app.main import check_password
+
+
+def test_check_password_corrected_result() -> None:
+    assert check_password("ізщфівИ0!") is False
+    assert check_password("") is False
+    assert check_password("Pass@word1Pass@word1") is False
+    assert check_password("Pass@word1") is True
