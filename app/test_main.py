9c8b1f9543da0b6c40fd1f9e488a8067f9704090
Mariia Horbova <gorbova21@gmail.com>
2023-06-15 22:41:32 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -7,26 +7,29 @@ from app.main import check_password
     "password,result",
     [
         ("Pass@word1", True),
+        ("VeryStr0ngP@ss", True),
+        ("Str0ng$", True),
+        ("a" * 20, False),
+        ("Sa1$", False),
         ("qwerty", False),
         ("Str@ng", False),
         ("PASSWORD", False),
         ("12345678", False),
         ("$@#&!-_", False),
-        ("a" * 150, False),
-        ("aaa", False),
 
     ],
     ids=[
-        "Result should equal True if password is Pass@word1",
+        "Result should equal True if password meets all requirements",
+        "Result should equal True if password meets all requirements",
+        "Result should equal True if password meets all requirements",
+        "Result should equal False if password is too long (>16 chars)",
+        "Result should equal False if password is too short (<8 chars)",
         "Result should equal False if password is lowercase only",
         "Result should equal False if password not contain digits",
         "Result should equal False if password is uppercase only",
         "Result should equal False if password is digits only",
         "Result should equal False if password is special characters only",
-        "Result should equal False if password is too long (>16 chars)",
-        "Result should equal False if password is too short (<8 chars)",
     ]
 )
-def test_should_correctly_convert_years(password: str,
-                                        result: bool) -> None:
+def test_valid_password(password: str, result: bool) -> None:
     assert check_password(password) == result
