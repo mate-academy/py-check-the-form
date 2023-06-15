310494195073094e3a871a3900e6652392ab4066
Oleksii Kolii <igravkompik@gmail.com>
2023-06-15 18:08:20 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,37 @@
-# write your code here
+import pytest
+
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize("value,expected", [
+    ("паролб5555[]", False),
+    ("' - - - -)(", False),
+    ("Pass@word1", True)
+])
+def test_password_should_contain_valid_symbols(value: str,
+                                               expected: bool
+                                               ) -> None:
+    assert check_password(value) == expected
+
+
+@pytest.mark.parametrize("value,expected", [
+    ("12W4!67", False),
+    ("123456789@bcDefgh", False),
+    ("K1ndaCoolP@sswrd", True)
+])
+def test_password_must_be_8_16_range(value: str,
+                                     expected: bool
+                                     ) -> None:
+    assert check_password(value) == expected
+
+
+@pytest.mark.parametrize("value,expected", [
+    ("P@ssword", False),
+    ("pASSword1", False),
+    ("p@ssword1", False)
+])
+def test_must_contain_at_least(value: str,
+                               expected: bool
+                               ) -> None:
+    assert check_password(value) == expected
