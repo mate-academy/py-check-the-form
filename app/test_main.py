21113c130b052764fff3beea589acb6616d5e838
Soloveyk1n <swain.volt@gmail.com>
2023-06-16 11:51:48 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,41 @@
-# write your code here
+from app.main import check_password
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "initial_password, expected_decision",
+
+    [
+        pytest.param(
+            'Pass',
+            False
+        ),
+        pytest.param(
+            'Pass@word1adadsadsadswdadwawaawdadw',
+            False
+        ),
+        pytest.param(
+            'Pass@word1',
+            True
+        ),
+        pytest.param(
+            'Pass@wordde',
+            False
+        ),
+        pytest.param(
+            'Password123',
+            False
+        ),
+        pytest.param(
+            'pass@word1',
+            False
+        ),
+        pytest.param(
+            "Pas@1",
+            False
+        ),
+    ]
+)
+def test_main(initial_password, expected_decision):
+    assert check_password(initial_password) == expected_decision
