27f3e8f311b027d756f647c4982108ec875f6788
Denys Gordiichuk <dd111320@gmail.com>
2023-06-21 17:38:48 +0300

    d

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,18 @@
-# write your code here
+import pytest
+
+from .main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, respond",
+    [
+        ("Pass@word1", True),
+        ("Pass@word1Pass@word1", False),
+        ("pass@word1", False),
+        ("Pass@word", False),
+        ("Password1", False),
+        ("P@a1", False),
+    ]
+)
+def test_password(password: str, respond: bool) -> None:
+    assert check_password(password) == respond
