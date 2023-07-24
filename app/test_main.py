a5db8c4ec9dda6f97f9d3c8ff2b0749c84e2f633
Dimo-n <dimon.chmara@gmail.com>
2023-07-24 13:18:46 +0300

    Solved

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,40 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password,is_valid",
+    [
+        pytest.param(
+            "Short", False,
+            id="test password greater min length"
+        ),
+        pytest.param(
+            "Too_loooong_passwoord", False,
+            id="test password less than max length"
+        ),
+        pytest.param(
+            "pass@word1", False,
+            id="test password contains uppercase letters"
+        ),
+        pytest.param(
+            "Pass@word1ю", False,
+            id="test password contains only latin letters"
+        ),
+        pytest.param(
+            "pass@word1", False,
+            id="test password contains uppercase letters"
+        ),
+        pytest.param(
+            "Pass@word", False,
+            id="test password contains digits"
+        ),
+        pytest.param(
+            "Passaword1", False,
+            id="test password contains special symbols"
+        ),
+    ]
+)
+def test_check_password(password: str, is_valid: bool) -> None:
+    assert check_password(password) == is_valid
