2910ee5b0048bfd3f1ec194bdd8e82f5933fc2a1
Mykhailo Korotia <mr.anderson528491@gmail.com>
2023-07-28 15:29:01 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -8,12 +8,16 @@ from app.main import check_password
         ("Pass@word1", True),
         ("TestPass@123", True),
         ("AbCdEfGh1$", True),
-        ("$ecretP@ss", True),
         ("qwerty", False),
         ("Str@ng", False),
         ("str", False),
         ("passwordistoolong1234567890987654321", False),
         ("abcdEFGH$", False),
+        ("pass@word1", False),
+        ("Test@123456789012345", False),
+        ("", False),
+        ("Pa5$$_", False),
+        ("Password12345", False),
     ]
 )
 def test_password(password: str, expected_result: bool) -> None:
