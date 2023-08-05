138ebf7afb2db99a13e060bb006ccee4e0bc2ec7
Illia Khomutov <ilyakhomutov613@gmail.com>
2023-08-06 00:15:52 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, approved", [
+        ("Pass@word1", True),
+        ("Pass@word1Pass@word1", False),
+        ("Pass@word", False),
+        ("Pass@word1", True),
+        ("1Pass@w", False),
+        ("1Passw5qwer", False),
+        ("1passw5qwer@", False)
+    ]
+)
+def test_check_password(password: str, approved: bool) -> None:
+    assert check_password(password) is approved
