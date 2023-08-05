2da140e7ca3d52cb1b435c85e8eb9f25f054f766
Illia Khomutov <ilyakhomutov613@gmail.com>
2023-08-06 00:17:12 +0300

    Solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -7,7 +7,6 @@ from app.main import check_password
         ("Pass@word1", True),
         ("Pass@word1Pass@word1", False),
         ("Pass@word", False),
-        ("Pass@word1", True),
         ("1Pass@w", False),
         ("1Passw5qwer", False),
         ("1passw5qwer@", False)
