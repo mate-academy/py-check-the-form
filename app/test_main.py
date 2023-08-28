d366f0e32c0d171588b406aa1bbbdb69bb8ac1a3
oreny <orenycdota@gmail.com>
2023-08-28 15:52:53 +0300

    Implemented tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        ("Pass@word1", True),
+        ("qwerty", False),
+        ("Str@ng", False),
+        ("Iamt00LooooongPassword!!!!!!helpme", False),
+        ("onlyChars!", False),
+        ("Ac1!", False),
+        ("WithoutSpec1al", False),
+        ("nouppercase01!", False)
+    ],
+)
+def test_check_password(password: str, result: bool) -> None:
+    assert check_password(password) == result
