1e312553d19a8e616d24bb59b9fda2980614f99c
Kateryna Zadorozhna <katya.zadorozhna.00@gmail.com>
2023-08-29 08:49:12 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,46 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password,expected",
+    [
+        pytest.param(
+            "Pass@word1",
+            True,
+            id="should return True when password is valid"
+        ),
+        pytest.param(
+            "Password1",
+            False,
+            id="should return False when no special symbols are in password"
+        ),
+        pytest.param(
+            "Pass@word",
+            False,
+            id="should return False when no digits are in password"
+        ),
+        pytest.param(
+            "password",
+            False,
+            id="should return False when no uppercase letters are in password"
+        ),
+        pytest.param(
+            "Word@1",
+            False,
+            id="should return False when password is too short"
+        ),
+        pytest.param(
+            "Password_password_password@1",
+            False,
+            id="should return False when password is too long"
+        ),
+        pytest.param(
+            "Password@1Ш",
+            False,
+            id="should return False when unacceptable symbol in password"
+        )
+    ]
+)
+def test_check_password(password: str, expected: bool) -> None:
+    assert check_password(password) == expected
