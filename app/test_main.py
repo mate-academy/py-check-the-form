b4256e5b335dfd6252dbd41c0ecf256f9f49cf4e
jinjo <vitaly.tyagunov@gmail.com>
2023-08-29 09:55:23 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -12,6 +12,5 @@ from app.main import check_password
                          ("AHFYGK132@a", True),
                          ("РПВРрывыврп@67", False),
                          ])
-def test_check_password(test_password, expected_result) -> None:
+def test_check_password(test_password: str, expected_result: bool) -> None:
     assert check_password(test_password) == expected_result
-
