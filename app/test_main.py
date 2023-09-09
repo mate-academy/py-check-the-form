eae13916aabb807f4b67bb9fb7976fab8dbf92b6
Elena Komarova <elenkomarova4@gmail.com>
2023-09-09 15:04:15 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,24 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected_password",
+    [
+        ("ab-3cdeFgh", True),
+        ("12345678", False),
+        ("kndfsa_123JHKHKASDBJHBAD", False),
+        ("s9_asIU", False),
+        ("kl&sd+$", False),
+        ("qwertyuiop", False),
+        ("ksdfUUHU_ao", False),
+        ("wer_23moien", False),
+        ("", False),
+        ("daf3468KJAOJ", False)
+    ]
+)
+def test_check_password(password: str,
+                        expected_password: bool,
+                        ) -> None:
+    assert check_password(password) == expected_password
