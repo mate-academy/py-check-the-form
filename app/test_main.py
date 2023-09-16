6594cf6384ffc9a4bb6e7e090c4c3400da851def
Ruslan <rusipbox@gmail.com>
2023-09-16 21:21:58 +0300

    Test1

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,30 @@
-# write your code here
+from app.main import check_password
+import pytest
+
+
+@pytest.mark.parametrize(
+    "password, expected",
+    [
+        ("Pass@word1", True),
+        ("qwerty", False),
+        ("Str@ng", False),
+        ("MateAc@damy1", True),
+        ("Python", False),
+        ((5, 2), ValueError),
+        ([], ValueError)
+
+    ]
+)
+def test_check_password(
+        password: str, 
+        expected: bool
+) -> None:
+    if isinstance(expected, bool):
+        assert check_password(password) == expected
+    else:
+        with pytest.raises(expected):
+            check_password(password)
+
+
+if __name__ == "__main__":
+    pytest.main()
