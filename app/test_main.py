e7665ea86339a4a52c2b418e401e09100c233a4c
Ruslan <rusipbox@gmail.com>
2023-09-17 16:13:47 +0300

    Test3(py-check-password)

--- app/test_main.py
+++ app/test_main.py
@@ -1,30 +1,37 @@
 from app.main import check_password
-import pytest
 
 
-@pytest.mark.parametrize(
-    "password, expected",
-    [
-        ("Pass@word1", True),
-        ("qwerty", False),
-        ("Str@ng", False),
-        ("MateAc@damy1", True),
-        ("Python", False),
-        ((5, 2), ValueError),
-        ([], ValueError)
-
-    ]
-)
-def test_check_password(
-        password: str,
-        expected: bool
-) -> None:
-    if isinstance(expected, bool):
-        assert check_password(password) == expected
-    else:
-        with pytest.raises(expected):
-            check_password(password)
+def test_valid_password() -> None:
+    assert check_password("Pass@word1") is True
 
 
-if __name__ == "__main__":
-    pytest.main()
+def test_too_short_password() -> None:
+    assert check_password("qwerty") is False
+
+
+def test_no_special_character() -> None:
+    assert check_password("Str0ngPass") is False
+
+
+def test_no_digit() -> None:
+    assert check_password("Special@Char") is False
+
+
+def test_no_uppercase() -> None:
+    assert check_password("lowercase@1") is False
+
+
+def test_valid_minimum_length() -> None:
+    assert check_password("Aa1$2") is False
+
+
+def test_valid_maximum_length() -> None:
+    assert check_password("Aa1$2Bb3@4Cc5-6") is True
+
+
+def test_too_long_password() -> None:
+    assert check_password("Aa1$2Bb3@4Cc5-6Dd7$8Ee9Ff0") is False
+
+
+def test_invalid_characters() -> None:
+    assert check_password("Invalid&Pass!") is False
