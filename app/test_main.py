8128873fc33dc7fd424bba27f7b36368d4602d36
Olgierrd <oolgierrd@gmail.com>
2023-09-20 13:28:57 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,30 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        ("Pass@word1", True),
+        ("Password1", False),
+        ("password1!", False),
+        ("Password!", False),
+        ("Sh0rt!", False),
+        ("to0lonGpassword!!", False),
+        ("two words", False)
+    ],
+    ids=[
+        "correct password",
+        "no special symbols",
+        "no upper case",
+        "no digits",
+        "too short",
+        "too long",
+        "password with spaces"
+    ]
+)
+def test_check_password(
+        password: str,
+        result: bool
+) -> None:
+    assert check_password(password) == result
