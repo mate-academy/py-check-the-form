d666d349a71fb0ed72d8d66cc6918f5adcb20691
Maksim Stahovskij <maksss196@gmail.com>
2023-09-22 23:59:19 +0300

    py-check-password

--- app/test_main.py
+++ app/test_main.py
@@ -2,33 +2,36 @@ import pytest
 
 from app.main import check_password
 
+
 @pytest.fixture()
-def password():
+def password() -> str:
     return "Pass@word1"
 
 
-def test_check_password_with_valid_password(password):
-    assert check_password(password) == True
+def test_check_password_with_valid_password(password: str) -> None:
+    assert check_password(password)
 
 
-def test_check_password_with_special_symbols(password):
+def test_check_password_with_special_symbols(password: str) -> None:
     password = "Password1"
-    assert check_password(password) == False
+    assert not check_password(password)
 
 
-def test_check_password_with_min_length(password):
+def test_check_password_with_min_length(password: str) -> None:
     password = "Pass@1"
-    assert check_password(password) == False
+    assert not check_password(password)
 
-def test_check_password_with_max_length(password):
+
+def test_check_password_with_max_length(password: str) -> None:
     password = "Pass@word1Pass@word1"
-    assert check_password(password) == False
+    assert not check_password(password)
 
 
-def test_check_password_with_out_digits(password):
+def test_check_password_with_out_digits(password: str) -> None:
     password = "Pass@word"
-    assert check_password(password) == False
+    assert not check_password(password)
 
-def test_check_password_with_out_uppercase(password):
+
+def test_check_password_with_out_uppercase(password: str) -> None:
     password = "pass@word1"
-    assert check_password(password) == False
+    assert not check_password(password)
