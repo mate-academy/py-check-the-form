93191afec3df6d1e8164ea387d1564448ba2a63a
Maksim Stahovskij <maksss196@gmail.com>
2023-09-22 23:43:23 +0300

    py-check-password

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,34 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+@pytest.fixture()
+def password():
+    return "Pass@word1"
+
+
+def test_check_password_with_valid_password(password):
+    assert check_password(password) == True
+
+
+def test_check_password_with_special_symbols(password):
+    password = "Password1"
+    assert check_password(password) == False
+
+
+def test_check_password_with_min_length(password):
+    password = "Pass@1"
+    assert check_password(password) == False
+
+def test_check_password_with_max_length(password):
+    password = "Pass@word1Pass@word1"
+    assert check_password(password) == False
+
+
+def test_check_password_with_out_digits(password):
+    password = "Pass@word"
+    assert check_password(password) == False
+
+def test_check_password_with_out_uppercase(password):
+    password = "pass@word1"
+    assert check_password(password) == False
