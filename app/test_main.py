54c9156cfc1275110812816b2906c73420039d07
Nadiia <nadiia.developer777@gmail.com>
2023-09-28 15:03:44 +0300

    Solution-py-check-password

--- app/test_main.py
+++ app/test_main.py
@@ -4,19 +4,19 @@ from app.main import check_password
 
 @pytest.mark.parametrize("password, expected", [
     ("Pass@word1", True),
-    ("Abcdefg1$", True),
-    ("P@ssw0rdA", True),
-    ("S3cr3t-$", True),
-    ("", False),
-    ("1234567", False),
-    ("Abcdefghijklmnopqrstuvwxyz", False),
-    ("noSpecial123", False),
-    ("OnlyLowerCase", False),
-    ("onlyuppercase", False),
-    ("12345678", False),
-    ("nopassword", False),
     ("qwerty", False),
-    ("Str@ng", False)
+    ("sH0$", False),
+    ("aass@word1", False),
+    ("PASSWORD", False),
+    ("Str@ng", False),
+    ("AbC12345@", True),
+    ("$pecialP@ss", False),
+    ("1234$AbCdEfG56789,", False),
+    ("NoSpecial123", False),
+    ("OnlyUpperCase", False),
+    ("12345678", False),
+    ("ToLong12345&passWord", False),
+    ("", False)
 ])
 def test_check_password(password: str, expected: bool) -> None:
     assert check_password(password) == expected
