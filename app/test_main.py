a9965a824f5b2bf484566283b08b446f51b89719
Vitalii Zhabskii <vitalik15031996@gmail.com>
2023-09-30 14:18:06 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,27 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize("password", [
+    "Pass@word1",
+    "AaBbC123$",
+    "AbCdEfGh$9-",
+    "AbcDefg$1",
+    "A@B1-cdEfgh"
+])
+def test_valid_passwords(password: any) -> None:
+    assert check_password(password) is True
+
+# Invalid passwords
+
+
+@pytest.mark.parametrize("password", [
+    "qwerty",
+    "Str@ng",
+    "Password12",
+    "abcdEFGH12345",
+    "invalid@pass&",
+    "P@$$word"
+])
+def test_invalid_passwords(password: any) -> None:
+    assert check_password(password) is False
