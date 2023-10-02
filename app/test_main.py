dba3299870742234116f921e00624549ffdd61bd
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-10-02 19:36:27 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,15 @@
 import pytest
 from app.main import check_password
 
+# Valid passwords
+
 
 @pytest.mark.parametrize("password", [
     "Pass@word1",
     "AaBbC123$",
     "AbCdEfGh$9-",
     "AbcDefg$1",
-    "A@B1-cdEfgh"
+    "A@B1cdEfgh_"
 ])
 def test_valid_passwords(password: any) -> None:
     assert check_password(password) is True
@@ -16,12 +18,12 @@ def test_valid_passwords(password: any) -> None:
 
 
 @pytest.mark.parametrize("password", [
-    "qwerty",
-    "Str@ng",
-    "Password12",
-    "abcdEFGH12345",
-    "invalid@pass&",
-    "P@$$word"
+    "qwerty",          # Less than 8 characters
+    "Str@ng",          # No digit
+    "Password12",      # No special character
+    "abcdEFGH12345",  # More than 16 characters
+    "invalidpass",     # No special character
+    "P@$$word",        # Less than 8 characters
 ])
 def test_invalid_passwords(password: any) -> None:
     assert check_password(password) is False
