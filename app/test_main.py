faa669e25a7471358c76acc87d272c0d80596e9a
Manynik <s.onufriyenko@gmail.com>
2023-10-11 17:57:31 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,41 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        pytest.param(
+            "Pass@word1",
+            True,
+            id="correct password"
+        ),
+        pytest.param(
+            "qwerty",
+            False,
+            id="should be at least 8 characters"
+        ),
+        pytest.param(
+            "Pass@word1qwerty123",
+            False,
+            id="should be maximum 16 characters"
+        ),
+        pytest.param(
+            "Pass@wordy",
+            False,
+            id="should contains at least 1 digit"
+        ),
+        pytest.param(
+            "Password12",
+            False,
+            id="should contains at least 1 special character"
+        ),
+        pytest.param(
+            "password@12",
+            False,
+            id="should contains at least 1 uppercase letter"
+        ),
+    ]
+)
+def test_check_password(password: str, result: bool) -> None:
+    assert check_password(password) == result
