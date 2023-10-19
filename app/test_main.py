a093cddb01fcc07844941ebacdad4d9aff58a469
D1st3f <dima.purig@gmail.com>
2023-10-19 17:03:45 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,18 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected",
+    [
+        ("Яasd32@sdf", False),
+        ("ASd", False),
+        ("ASdASdASdASdASdASd", False),
+        ("Abcd12345", False),
+        (",,,,,,,,,,", False),
+        ("Abcd12345@", True)
+    ]
+)
+def test_check_password(password: str, expected: bool) -> None:
+    assert check_password(password) == expected
