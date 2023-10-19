0ba1e85d137dd4d57f1c7d9a54e0101f4a91b50d
Ihor Prochnickiy <2prochigor@gmail.com>
2023-10-19 20:50:47 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+from app.main import check_password
+
+
+def test_correct_password() -> None:
+    assert check_password("Arh6-3shah") is True
+
+
+def test_password_without_digits() -> None:
+    assert check_password("Mrjerks1&") is False
+
+
+def test_password_with_short_password() -> None:
+    assert check_password("1-rB") is False
+
+
+def test_too_long_password() -> None:
+    assert check_password("zxcvbnAsfvf6vbfd2-") is False
+
+
+def test_password_without_special_characters() -> None:
+    assert check_password("Arb56jmd") is False
+
+
+def test_password_without_upper_letter() -> None:
+    assert check_password("qwe123-f-ff4") is False
