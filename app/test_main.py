0d2629bd7f5311a98cf22fa3a318e42523e4549d
Rost <rv4@ukr.net>
2023-10-19 23:11:41 +0300

    py-check-password Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result", [
+        ("Qw@1ty", False),
+        ("Qwertyuiopry1", False),
+        ("Qwertyui$", False),
+        ("Qwert@івап1", False),
+        ("qwe1@rty", False),
+        ("Qw1@rtyujhgfdcvbn", False),
+        ("Qwerty1@", True)
+    ],
+    ids=[
+        "Must be at least 8 characters",
+        "Must contain at least one special character",
+        "Must contain at least one digit",
+        "Must contain only Latin letters",
+        "Must contain at least one upper case",
+        "Too long. Maximum 16 characters",
+        "Password is accepted!"
+    ]
+)
+def test_check_password(password: str, result: bool) -> None:
+    assert check_password(password) == result
