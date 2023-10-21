3f1f29e227af2d7af94f4582e2159535d822f18c
Dmytro Petrykiv <petrykiv.dmytro19@gmail.com>
2023-10-21 11:18:08 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,18 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, is_valid",
+    [
+        ("Яasd32@sdf", False),
+        ("ASd", False),
+        ("ASdASdASdASdASdASd", False),
+        ("Abcd12345", False),
+        (",,,,,,,,,,", False),
+        ("Abcd12345@", True)
+    ]
+)
+def test_check_password(password: str, is_valid: bool) -> None:
+    assert check_password(password) == is_valid
