f3d9d645049fbd98184a5581115382eb0d41b9b8
Vladyslav Shatrovskyi <shatrovskyi.vlad@gmail.com>
2023-10-22 10:09:01 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -13,3 +13,28 @@ from app.main import check_password
 )
 def test_valid_password(password: str, result: bool) -> None:
     assert check_password(password) == result
+
+
+def test_should_check_max_length() -> None:
+    assert not check_password("Pass@word134547hyy"), \
+        "Password max length should be 17 symbols"
+
+
+def test_should_check_min_length() -> None:
+    assert not check_password("P@wrd13"), \
+        "Password min length should be 8 symbols"
+
+
+def test_should_check_digit() -> None:
+    assert not check_password("P@wrdurttd"), \
+        "Password should contain digits"
+
+
+def test_should_check_upper_letter() -> None:
+    assert not check_password("p@wrdurttd1112"), \
+        "Password should contain upper letter"
+
+
+def test_should_check_special_symbols() -> None:
+    assert not check_password("Pawrdurttd1112"), \
+        "Password should contain special symbols"
