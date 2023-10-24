0db37adf35964ebe34e204132df37d859b6172e1
Alex Ilchenko <alec.ilchenko@gmail.com>
2023-10-25 02:27:36 +0400

    Fix flake8 errors

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,21 @@
 from app.main import check_password
 
 
-def test_short_password():
+def test_short_password() -> None:
     assert check_password("Pa@rd1") is False
 
 
-def test_too_long_password():
+def test_too_long_password() -> None:
     assert check_password("Pass@word11111111") is False
 
 
-def test_without_upper_case():
+def test_without_upper_case() -> None:
     assert check_password("pass@word1") is False
 
 
-def test_check_digits():
+def test_check_digits() -> None:
     assert check_password("Pass@wordd") is False
 
 
-def test_check_special_symbol():
+def test_check_special_symbol() -> None:
     assert check_password("Passsword1") is False
