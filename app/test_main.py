3cc27bb71bbda4848010976d992ef49820c118e9
ANMI <migunov1au@gmail.com>
2023-11-04 16:53:20 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,18 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        ("Pass@word1", True),
+        ("Qwer!y1", False),
+        ("!Str@ngButT00Long!", False),
+        ("Justaw@rd", False),
+        ("just@w0rd", False),
+        ("Justaw0rd", False)
+    ]
+)
+def test_general_functionality(password: str, result: bool) -> None:
+    assert check_password(password) is result
