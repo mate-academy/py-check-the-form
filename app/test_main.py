1e324c061cd53707a429a478731b6789bb0b139a
DanSheremeta <danylo3002@gmail.com>
2023-11-05 02:30:44 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+import pytest
+
+from .main import check_password
+
+
+@pytest.mark.parametrize(
+    "password,expected",
+    [
+        ("Mate1_", False),
+        ("Maaaaaaaateeeee111122222____", False),
+        ("Mateacademy1_", True),
+        ("mateacademy1_", False),
+        ("Mateacademy1_", True),
+        ("Mateacademy1", False),
+        ("Mateacademy1_", True),
+        ("Mateacademy_", False),
+    ]
+)
+def test_password_has_all_requirements(
+        password: str,
+        expected: bool
+) -> None:
+    assert check_password(password) == expected
