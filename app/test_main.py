e13fa691901f30861e628f4cfc83e511e96b92cd
Andrii Malovanyi <malovaniy.an@gmail.com>
2023-11-09 11:40:02 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,15 +6,17 @@ from app.main import check_password
 @pytest.mark.parametrize(
     "password, expected",
     [
-        ("Pass@word1" ,True),
+        ("Pass@word1", True),
         ("Pass@word", False),
         ("pass@word1", False),
         ("Password1", False),
         ("Pass$word1", True),
-        ("Pass@word1", True),
+        ("Pa@w1", False),
         ("Pass#word1", True),
         ("Pass&word1", True),
+        ("Pass@word1", True),
         ("Pass!word1", True),
+        ("Pass!wortfrthhrhrd1", False),
         ("Pass-word1", True),
         ("Pass_word1", True)
     ]
