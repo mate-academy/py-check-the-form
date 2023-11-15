8a54cb51f33c8ed35edf84bbd4770e7fad0edbbe
Aeros72 <pashamelnyk2004@gmail.com>
2023-11-15 16:13:11 +0200

    Implemented py-check-password

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, value",
+    [
+        ("Qwerty$@#&!-_1234", False),
+        ("Qwer!3#", False),
+        ("qwerty#7j", False),
+        ("QwertY@uio_", False),
+        ("Qwerty1235", False)
+    ]
+)
+def test_check_password(
+        password: str,
+        value: bool
+) -> None:
+    assert check_password(password) == value
