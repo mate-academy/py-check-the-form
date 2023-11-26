c536a586843fcb5bea3eb35602efdf47d2b0017b
Marinel444 <380978907444m@gmail.com>
2023-11-26 12:45:32 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,48 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+class TestCheckPassword:
+    @pytest.mark.parametrize(
+        "password, result",
+        [
+            pytest.param(
+                "Мn!1",
+                False,
+                id="Should return False"
+            ),
+            pytest.param(
+                "Попробуем_this",
+                False,
+                id="not all letters are in English"
+            ),
+            pytest.param(
+                "Password1",
+                False,
+                id="without special characters"
+            ),
+            pytest.param(
+                "passwor1d_",
+                False,
+                id="without capital letter"
+            ),
+            pytest.param(
+                "My_password1",
+                True,
+                id="should return True"
+            ),
+            pytest.param(
+                "My_password1isverylongbatitisgoodsfdre",
+                False,
+                id="should return True"
+            ),
+            pytest.param(
+                "Qwertyu!iop1asdf",
+                True,
+                id="should return False ven 16 symbol only"
+            )
+        ]
+    )
+    def test_check_password(self, password: str, result: bool) -> None:
+        assert check_password(password) == result
