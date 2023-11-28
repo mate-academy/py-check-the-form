abf6c7c668c1e2eb1f49c098315292cfde0fe555
Roman Shupak <romanshupak808@gmail.com>
2023-11-28 21:05:19 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,55 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected_result",
+    [
+        pytest.param(
+            "Pass@word1",
+            True,
+            id="password is valid"
+        ),
+        pytest.param(
+            "password",
+            False,
+            id="password is not correct"
+        ),
+        pytest.param(
+            "StrongPassword1",
+            False,
+            id="password is not correct"
+        ),
+        pytest.param(
+            "Short1!",
+            False,
+            id="password is not correct"
+        ),
+        pytest.param(
+            "short11!",
+            False,
+            id="password is not correct"
+        ),
+        pytest.param(
+            "Tolongpassword11!",
+            False,
+            id="password is not correct"
+        ),
+        pytest.param(
+            "Nodigits!",
+            False,
+            id="password is not correct"
+        )
+    ]
+)
+def test_check_password(
+    password: str,
+    expected_result: bool
+) -> None:
+
+    result = check_password(password)
+    assert result == expected_result, (
+        f"Expected result: {expected_result}, "
+        f"Actual result: {result}"
+    )
