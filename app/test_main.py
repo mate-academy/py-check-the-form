03589e6a2cec7af1a10363f41ea6cd28c74bfb85
Vlad <mr.darmstadtium@gmail.com>
2023-12-07 19:04:44 +0100

    wrote all code

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrise(
+    "password, result",
+    [
+        ("aB3!", False),
+        ("nhaaaaaB3", False),
+        ("nhaaaaaB!", False),
+        ("nhaaaab3!", False),
+        ("Spam2pam@pamSPAM666", False),
+        ("nOrma1p@rol", True)
+    ]
+)
+def test_check_password(
+        password: str,
+        result: bool) -> None:
+
+    assert(
+        check_password(password) == result
+    )
