4c1e5b214d5edf8269fec16bf5ba41a6a3f15cfb
Viktor Rura <ruravictor@gmail.com>
2023-12-08 13:13:53 +0000

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,33 @@
-# write your code here
+from .main import check_password
+
+
+def test_valid_password() -> None:
+    assert check_password("Pass@word1") is True
+
+
+def test_short_password() -> None:
+    assert check_password("Abc@123") is False
+
+
+def test_long_password() -> None:
+    assert check_password("Abc@123456789012345") is False
+
+
+def test_no_uppercase() -> None:
+    assert check_password("abc@1234") is False
+
+
+def test_no_digit() -> None:
+    assert check_password("Abc@defgh") is False
+
+
+def test_no_special_character() -> None:
+    assert check_password("Abcdefgh1234") is False
+
+
+def test_invalid_character() -> None:
+    assert check_password("Abc@1234*") is False
+
+
+def test_non_latin_alphabet() -> None:
+    assert check_password("Секрет@123") is False
