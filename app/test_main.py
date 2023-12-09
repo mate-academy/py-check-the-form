646e9e697eeff6631fc34274cc98c0d3def3d677
Anton Konyk <antonkonyk.com>
2023-12-09 11:19:50 +0100

    Solution_09122023

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected",
+    [
+        ("ABCDEFGHIJKLMN1$", True),
+        ("OPQRSTUVWXYZ2@", True),
+        ("OPQRSTUVWXYZ3#", True),
+        ("OPQRSTUVWXYZ4&", True),
+        ("OPQRSTUVWXYZ5!", True),
+        ("OPQRSTUVWXYZ6-", True),
+        ("7890OPQRSTUVWXY-", True),
+        ("7890opqrstuvwxy-", False),
+        ("opqrStuvwxy-", False),
+        ("7890opqrstuvwxY", False),
+        ("VWXYZ4&", False),
+        ("ABCDEFGHIJKLMN1$3", False),
+    ]
+)
+def test_check_password(password: str,
+                        expected: bool
+                        ) -> None:
+    assert check_password(password) == expected
