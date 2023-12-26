d3b6128ddc02371f157b1a40b4a341be1bb22166
Maxim Voytkevichus <m4k51m15k4m@gmail.com>
2023-12-26 21:43:51 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,29 @@
-# write your code here
+import unittest
+from app.main import check_password
+
+
+class TestCheckPassword(unittest.TestCase):
+
+    def test_valid_password(self) -> None:
+        self.assertTrue(check_password("Pass@word1"))
+
+    def test_invalid_length_short(self) -> None:
+        self.assertFalse(check_password("Short@1"))
+
+    def test_invalid_length_long(self) -> None:
+        self.assertFalse(check_password("Pass@word1wwwwwww"))
+
+    def test_invalid_no_digit(self) -> None:
+        self.assertFalse(check_password("NoDigit@Special"))
+
+    def test_invalid_no_special_char(self) -> None:
+        self.assertFalse(check_password("NoSpecialChar123"))
+
+    def test_invalid_no_uppercase(self) -> None:
+        self.assertFalse(check_password("nouppercase@123"))
+
+    def test_invalid_invalid_char(self) -> None:
+        self.assertFalse(check_password("InvalidChar$"))
+
+    def test_invalid_multiple_special_chars(self) -> None:
+        self.assertFalse(check_password("TooManySpecialChars@!"))
