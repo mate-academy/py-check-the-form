adb1b90665ef7cd752de986bdf3ac5f97fcb8de4
Borys Kirichenko <boryskirichenko99@gmail.com>
2024-01-04 12:05:36 +0100

    all tests passed

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,43 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result", [
+        pytest.param(
+            "Pass@word1",
+            True,
+            id="should return true"
+        ),
+        pytest.param(
+            "P@word1",
+            False,
+            id="should return false when pass less then 8 symbols"
+        ),
+        pytest.param(
+            "P@w88888888ord1fds",
+            False,
+            id="should return false when pass more then 17 symbols"
+        ),
+        pytest.param(
+            "Pass@word",
+            False,
+            id="should return false when pass dont have digit"
+        ),
+        pytest.param(
+            "Password1",
+            False,
+            id="should return false when pass dont have special symbols"
+        ),
+        pytest.param(
+            "pass@word1",
+            False,
+            id="should return false when pass dont have upper case"
+        ),
+
+    ]
+)
+def test_check_password(password: str, result: bool) -> None:
+    assert(
+        check_password(password) == result
+    )
