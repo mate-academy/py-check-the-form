14a1681e5b1a9adeea7b868fdf34ffe74dbdbf4d
Volodymyr Yablonskyi <wowa2807@gmail.com>
2024-01-07 15:02:18 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -32,27 +32,31 @@ def invalid_password_no_uppercase() -> None:
     return "password@123"
 
 
-def test_check_password_valid(valid_password) -> None:
+def test_check_password_valid(valid_password: any) -> None:
     assert check_password(valid_password) is True
 
 
-def test_check_password_invalid_too_short(invalid_password_too_short) -> None:
+def test_check_password_invalid_too_short(invalid_password_too_short: any) -> None:
     assert check_password(invalid_password_too_short) is False
 
 
-def test_check_password_invalid_too_long(invalid_password_too_long) -> None:
+def test_check_password_invalid_too_long(invalid_password_too_long: any) -> None:
     assert check_password(invalid_password_too_long) is False
 
 
-def test_check_password_invalid_no_digit(invalid_password_no_digit) -> None:
+def test_check_password_invalid_no_digit(
+        invalid_password_no_digit: any
+) -> None:
     assert check_password(invalid_password_no_digit) is False
 
 
-def test_check_password_invalid_no_special_character(invalid_password_no_special_character) -> None:
+def test_check_password_invalid_no_special_character(
+        invalid_password_no_special_character: any
+) -> None:
     assert check_password(invalid_password_no_special_character) is False
 
 
-def test_check_password_invalid_no_uppercase(invalid_password_no_uppercase) -> None:
+def test_check_password_invalid_no_uppercase(
+        invalid_password_no_uppercase: any
+) -> None:
     assert check_password(invalid_password_no_uppercase) is False
-
-
