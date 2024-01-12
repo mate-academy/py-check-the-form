21149a5747cde2053d24ea215c11ec5a9f220bd7
Denys Sidorov <zondayttop1ggg@gmail.com>
2024-01-12 20:57:07 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,41 +1,18 @@
+import pytest
 from app.main import check_password
 
 
-def test_valid_password() -> None:
-    assert check_password("Pass@word1") is True
-
-
-def test_invalid_short_password() -> None:
-    assert check_password("qwerty") is False
-
-
-def test_invalid_no_digit_password() -> None:
-    assert check_password("Str@ng") is False
-
-
-def test_invalid_no_special_char_password() -> None:
-    assert check_password("Password123") is False
-
-
-def test_invalid_no_uppercase_password() -> None:
-    assert check_password("pass@word1") is False
-
-
-def test_invalid_long_password() -> None:
-    assert check_password("TooLongPassword123456") is False
-
-
-def test_valid_maximum_length_password() -> None:
-    assert check_password("MaxLenPass@word1") is True
-
-
-def test_invalid_exceed_maximum_length_password() -> None:
-    assert check_password("ExceedMaxLenPass@word1") is False
-
-
-def test_invalid_restricted_special_char_password() -> None:
-    assert check_password("RestrictedChar$1") is True
-
-
-def test_valid_custom_special_char_password() -> None:
-    assert check_password("Custom@SpecialChar1") is False
+@pytest.mark.parametrize("password, expected_result", [
+    ("Pass@word1", True),
+    ("qwerty", False),
+    ("Str@ng", False),
+    ("Password123", False),
+    ("pass@word1", False),
+    ("TooLongPassword123456", False),
+    ("MaxLenPass@word1", True),
+    ("ExceedMaxLenPass@word1", False),
+    ("RestrictedChar$1", True),
+    ("Custom@SpecialChar1", False),
+])
+def test_check_password(password: str, expected_result: bool) -> None:
+    assert check_password(password) == expected_result
