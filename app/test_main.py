922454f52a0d05b192d76125afbc069e1193c688
Oleksii Mileiko <alexexile1987@gmail.com>
2024-01-14 00:44:48 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, except_result",
+    [
+        ("Pass@word1", True),
+        ("qwerty1234", False),
+        ("S@ng1", False),
+        ("Strng@1234567890asdfasdf", False),
+        ("Pass@word", False),
+        ("password1", False),
+        ("Password1", False),
+        ("pass@word1", False),
+    ]
+)
+def test_check_password(password: str, except_result: bool) -> None:
+    result = check_password(password)
+    assert result == except_result
