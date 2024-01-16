0dbdbe4433b6a9c1bbfe317dbf9f343b84bb61f9
Vladyslav Pliashechnyk <vlad.pliashechnyk@gmail.com>
2024-01-16 23:49:36 +0200

    py-check-password

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, boolean_value",
+    [
+        ("Pass@word1", True),
+        ("Pass@word", False),
+        ("Pass@word1wertyuq", False),
+        ("Password1w", False),
+        ("P@word1", False),
+        ("pass@word1", False),
+    ]
+)
+def test_should_output_correct_boolean_value(
+    password: str,
+    boolean_value: bool
+) -> None:
+    assert check_password(password) == boolean_value
