4d78fa6f94ba6ea09a5e94b934e371bc7e97effb
BirovShandor <birovs777@gmail.com>
2024-01-21 00:19:10 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,9 @@
-# write your code here
+from app.main import check_password
+
+
+def test_if_the_password_is_correct() -> None:
+    assert check_password("Pass@word1") is True
+
+
+def test_if_the_password_isnt_correct() -> None:
+    assert check_password("qwerty") is False
