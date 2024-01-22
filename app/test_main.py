d1a92abbdb64101907dcfaf321b3978d9ce16232
Veronika <veronikaboeva935@gmail.com>
2024-01-22 22:31:04 +0100

    Solution 22.01.2024

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,30 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "word,output",
+    [
+        ("Pass@word1", True),
+        ("Pas1@", False),
+        ("qwerty", False),
+        ("Qwerty62@jdkcmpwpdmxk", False),
+        ("Passw@rd", False),
+        ("password12@", False),
+        ("Password1", False)
+    ],
+    ids=(
+        "password is valid",
+        "password it too short",
+        "password does not match any of rules",
+        "password should be any longer than 16 characters",
+        "password must have digits",
+        "need to have upper letter",
+        "there might be a special symbol"
+    )
+)
+def test_password(
+        word: str,
+        output: bool
+) -> None:
+    assert check_password(word) == output
