b21c79eaa751a9b503b5e2158c2453e6941bb3b5
Zoresław Dmytryszyn <zoreslav.dmytryshyn@gmail.com>
2024-01-25 15:22:38 +0100

    solve check-password

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,49 @@
-# write your code here
+import pytest
+
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        pytest.param(
+            "AAAbbb11@@_ЖЖЖ",
+            False,
+            id="Should only accept letters of the Latin alphabet"
+        ),
+        pytest.param(
+            "AAAbbb11@@_???",
+            False,
+            id="Should accept only special characters from '$@#&!-_'"
+        ),
+        pytest.param(
+            "Ab1@",
+            False,
+            id="Should be at least 8 characters"
+        ),
+        pytest.param(
+            "AAAbbbCCC11@@__!!!!!",
+            False,
+            id="Should be maximum 16 characters inclusive"
+        ),
+        pytest.param(
+            "AAAbbb11",
+            False,
+            id="Should contain at least 1 special character"
+        ),
+        pytest.param(
+            "AAAbbb@@",
+            False,
+            id="Should contain at least 1 digit"
+        ),
+        pytest.param(
+            "aaabbb11@@",
+            False,
+            id="Should contains at least 1 uppercase letter"
+        ),
+        ("AAAbbb11@@_", True),
+    ]
+)
+def test_check_password(password: str, result: bool) -> None:
+    assert check_password(password) == result
