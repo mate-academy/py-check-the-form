4ceca7365322f1b4c1dbab0feacfbafac3fb7371
IvanGuculyak <ivan.guculyak@gmail.com>
2024-02-06 19:32:13 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    ("password", "validation"),
+    [
+        ("Pass@word1", True),
+        ("P1@erty", False),
+        ("Qwerty@long", False),
+        ("12@password", False),
+        ("Str@ng", False),
+        ("@12Verylongpasswordtome", False)
+    ]
+)
+def test_check_password(
+        password: str,
+        validation: bool
+) -> None:
+    assert check_password(password) == validation
