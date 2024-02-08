9ef19449acd342da1c822f54c8c20c4ccc15ee44
Eugene Protsenko <guitaaarpro@gmail.com>
2024-02-08 10:03:04 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,8 +5,10 @@ from app.main import check_password
 @pytest.mark.parametrize(
     "password,expected_result",
     [
-        ("ksas", False),
-        ("sldjalskdjlkjdaadf", False),
+        ("Ss1@s", False),
+        ("Aldj1lsk&jlkjdaadf____", False),
+        ("asdd@12212", False),
+        ("sddfsfsdffff", False),
         ("Msds1asd&_", True),
         ("Asdsdf$sdf", False),
         ("1123123123", False),
