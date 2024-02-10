b79a91270e0d28289d6518ddd00a47bda3c853ee
sergey <38cerg12021979@jmal.com>
2024-02-10 08:03:27 +0200

    10_02_24_03

--- app/test_main.py
+++ app/test_main.py
@@ -6,24 +6,11 @@ from app.main import check_password
     "password, expected_result",
     [
         ("Pass@word1", True),
-        ("qwerty", False),
-        ("Str@ng", False),
         ("LongerPassword123!", False),
-        ("Short1!", False),
         ("NoSpecialChar123", False),
         ("NoDigit&Special", False),
-        ("OnlyLowercaseletters", False),
-        ("OnlyUppercaseletters", False),
-        ("OnlySpecialCharacters@", False),
-        ("OnlyDigits123456", False),
-        ("Valid@Passw0rd", True),
-        ("AnotherValidPassw0rd", False),
-        ("AValidPasswordWith$ymbols!", False),
-        ("Sh0rt!", False),
-        ("Toolongpasswordwithmorethan16characters", False),
-        ("приветпарольь", True),
-        ("qwertyuiop", False),
-
+        ("Pas_2", False),
+        ("ppercaselett@4", False)
     ]
 )
 def test_check_password(password: str, expected_result: bool) -> None:
