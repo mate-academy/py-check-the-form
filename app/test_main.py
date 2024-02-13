8f61567fcba99b0f4f369afbc3c7034fcf96aaef
srx <srx92@protonmail.com>
2024-02-13 22:28:05 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,22 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    ("passwd", "confirmation"),
+    [
+        ("P4$$w0rD", True),
+        ("Password123!", True),
+        ("Qw3rt!", False),
+        ("4w3S0m3P4$$w0rD", True),
+        ("4w3S0m3P4$$w0rD%^&", False),
+        ("P4$$w0rd%^*", False),
+        ("Пароль!23", False),
+    ]
+)
+def test_check_password(
+        passwd: str,
+        confirmation: bool
+) -> None:
+    assert check_password(passwd) == confirmation
