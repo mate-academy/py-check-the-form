03c3824e1291f291883cb4217d05fa813a6b1ea2
RomanHlodann <roman.hlodann@gmail.com>
2024-02-19 19:13:35 +0200

    implemented tests for func check_password

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password,result",
+    [
+        ("Pass@word1", True),
+        ("asc_q1@Werty", True),
+        ("Pass@qwerty", False),
+        ("Лщs@qwerty", False),
+        ("asc_q1Wertyrrrrrr", False),
+        ("P1@w", False),
+        ("pass@word1", False),
+        ("ascq1Werty", False),
+    ]
+)
+def test_correct_password_check(
+        password: str,
+        result: bool
+) -> None:
+    assert check_password(password) == result
