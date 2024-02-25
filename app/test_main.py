1bc0f3594669bfcfd452340f3d139af03dbfa6e4
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-02-25 11:00:17 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,36 @@
-# write your code here
+from app.main import check_password
+
+
+def test_returns_true_for_valid_password() -> None:
+    result = check_password("Pass@word1")
+    assert result is True
+
+
+def test_returns_false_is_less_than_8_characters() -> None:
+    result = check_password("Pa@d1")
+    assert result is False
+
+
+def test_returns_false_if_cyrillic_letter() -> None:
+    result = check_password("Pфss@word1")
+    assert result is False
+
+
+def test_returns_false_if_too_many_characters() -> None:
+    result = check_password("Pass@word11234567")
+    assert result is False
+
+
+def test_returns_false_if_doesnt_contain_digit() -> None:
+    result = check_password("Pass@word")
+    assert result is False
+
+
+def test_returns_false_if_doesnt_contain_special_char() -> None:
+    result = check_password("Password1")
+    assert result is False
+
+
+def test_returns_false_if_doesnt_contain_uppercase_letter() -> None:
+    result = check_password("pass@word1")
+    assert result is False
