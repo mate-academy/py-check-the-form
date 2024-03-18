aa6ec0a83527df5ad88e397c3b746213844ea575
Marian Kostiuk <131563162+kostiukmkalne@users.noreply.github.com>
2024-03-17 21:38:38 -0500

    one more solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,10 +6,12 @@ from app.main import check_password
     "password, expected",
     [
         ("Pass@word1", True),
-        ("qwerty", False),
-        ("Str@ng", False),
-        ("Mate_academy19908", False),
-
+        ("Pass@word", False),
+        ("Pass@r1", False),
+        ("pass@word1", False),
+        ("Pass@word1pass@word1", False),
+        ("Password1", False),
+        ("Mate_academy_1998065-+", False),
     ]
 )
 def test_check_password(password: str, expected: bool) -> None:
