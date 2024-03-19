2dea5af3b3f9ad7d0d61521c09bbde41f57a8f74
donbas777 <renary34@gmail.com>
2024-03-19 20:52:18 +0200

    'SOlution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+from app.main import check_password
+
+
+def test_normal_password() -> None:
+    assert check_password("Pass@word1") is True
+
+
+def test_less_8_symbols() -> None:
+    assert check_password("Q1@rty") is False
+
+
+def test_more_16_symbols() -> None:
+    assert check_password("Qwe@rtyqwertyqw1erty") is False
+
+
+def test_without_number() -> None:
+    assert check_password("Qwer@tyqwer") is False
+
+
+def test_without_big_letter() -> None:
+    assert check_password("qwe@rwwert1") is False
+
+
+def test_witout_special_symbol() -> None:
+    assert check_password("Q1wertyqwe") is False
