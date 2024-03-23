a84c1dfcb1c9f06b6d1162b05cab509772e27e91
Oles Shulzhenko <simplemanua@gmail.com>
2024-03-23 16:50:08 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,41 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password,expected_result",
+    [
+        pytest.param(
+            "Pass@word1",
+            True,
+            id="valid password"
+        ),
+        pytest.param(
+            "$qwerty2",
+            False,
+            id="not upper"
+        ),
+        pytest.param(
+            "9991Asdf",
+            False,
+            id="not special symbol"
+        ),
+        pytest.param(
+            "GeniusAA#",
+            False,
+            id="not digit"
+        ),
+        pytest.param(
+            "R2&",
+            False,
+            id="short passwords"
+        ),
+        pytest.param(
+            "Ukraine1991!",
+            False,
+            id="long passwords"
+        )
+    ]
+)
+def test_check_password(password: str, expected_result: bool) -> None:
+    assert check_password(password) == expected_result
