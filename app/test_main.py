5207fab90df1c8e574857be42b015a5f84154060
sudakolha <sudakolha@outlook.com>
2024-03-25 22:17:36 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -12,7 +12,7 @@ from app.main import check_password
         ("Pass@word1Pass@word1", False),
         ("Pas@1", False),
         ("Qwerty-qwerty@", False),
-        ("Qwerty_qwerty1", False), ]
+        ("Qwertyqwerty1", False), ]
 )
 def test_password(password: str, result: bool) -> None:
     assert check_password(password) == result
