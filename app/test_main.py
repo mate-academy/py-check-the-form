6115b05b6a614c8f15282dc1bac08b29238f77b9
sudakolha <sudakolha@outlook.com>
2024-03-26 15:30:21 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,18 +1,25 @@
 import pytest
-
-
 from app.main import check_password
 
 
 @pytest.mark.parametrize(
     "password, result",
-    [("Pass@word1", True),
-        ("qwerty@1", False),
-        ("Str@ng", False),
-        ("Pass@word1Pass@word1", False),
-        ("Pas@1", False),
-        ("Qwerty-qwerty@", False),
-        ("Qwertyqwerty1", False), ]
+    [
+        pytest.param("Pass@word1", True,
+                     id="valid password"),
+        pytest.param("Pass1@r", False,
+                     id="short password"),
+        pytest.param("Str@ngShpomnmt1hyt", False,
+                     id="password is too long"),
+        pytest.param("П@роль123", False,
+                     id="password contains invalid characters"),
+        pytest.param("P@ssword", False,
+                     id="password must contain at least one number"),
+        pytest.param("p@ssword1", False,
+                     id="password must contain at least one capital letter"),
+        pytest.param("Password1", False,
+                     id="password must contain at least one special character")
+    ]
 )
-def test_password(password: str, result: bool) -> None:
+def test_check_password(password: str, result: bool) -> None:
     assert check_password(password) == result
