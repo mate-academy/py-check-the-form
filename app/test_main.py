82d99b63cab7e00416676d623a86dcc27388103e
Mariia Sydoruk <marisydoruk12@gmail.com>
2024-03-28 18:58:04 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,35 +1,45 @@
-import pytest
 from app.main import check_password
 
 
-@pytest.fixture
-def pass_template() -> str:
-    return "123455678"
-
-
 def test_valid_password() -> None:
-    assert check_password("Pass@word1")
+    assert check_password("Pass@word1") is True
 
 
-def test_should_check_min_length(pass_template: str) -> None:
-    assert len(pass_template) >= 8 and len(pass_template) <= 16
+def test_password_too_short() -> None:
+    assert check_password("1234567") is False
 
 
-def test_should_check_digit(pass_template: str) -> None:
-    assert not check_password("Str@ng")
+def test_password_too_long() -> None:
+    assert check_password("abcdefghijklmnopq") is False
 
 
-def test_should_check_special_character() -> None:
-    assert not check_password("Password1")
+def test_missing_uppercase() -> None:
+    assert check_password("pass@word1") is False
 
 
-def test_should_check_upper_letter() -> None:
-    assert not check_password("pass@word1")
+def test_missing_digit() -> None:
+    assert check_password("Password!") is False
 
 
-def test_should_check_max_length() -> None:
-    assert not check_password("ThisIsTooLongPassword@1234")
+def test_missing_special_character() -> None:
+    assert check_password("Password1") is False
 
 
-def test_empty_password() -> None:
-    assert not check_password("")
+def test_valid_minimum_length() -> None:
+    assert check_password("Aa1$ffff") is True
+
+
+def test_valid_maximum_length() -> None:
+    assert check_password("AbC1234$@#&-_") is True
+
+
+def test_invalid_character() -> None:
+    assert check_password("Pa$$wórd1") is False
+
+
+def test_valid_with_all_special_characters() -> None:
+    assert check_password("Pa$$w@rd1") is True
+
+
+def test_invalid_with_all_special_characters() -> None:
+    assert check_password("Pa$$w@rd") is False
