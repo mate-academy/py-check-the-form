64be362b2e9d51bf276dd77382756c24ec22c99f
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-04-02 18:50:29 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,35 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "new_input,expected_output",
+    [
+        (
+            "Pass@word1", True
+        ),
+        (
+            "qwertynice", False
+        ),
+        (
+            "Str@ng", False
+        ),
+        (
+            "hellotoolongpassword@1", False
+        ),
+        (
+            "Hellospecial", False
+        ),
+        (
+            "he11o@234", False
+        ),
+        (
+            "Aaaaa@1", False
+        ),
+        (
+            "Hello@one", False
+        )
+    ]
+)
+def test_valid_password(new_input: str, expected_output: bool) -> None:
+    assert check_password(new_input) is expected_output
