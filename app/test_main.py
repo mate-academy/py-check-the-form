83627286c26f800e3328958adb00239e857a933a
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-04-02 18:53:55 +0200

    deleted pytest.param

--- app/test_main.py
+++ app/test_main.py
@@ -5,14 +5,14 @@ from app.main import check_password
 @pytest.mark.parametrize(
     "new_input,expected_output",
     [
-        pytest.param("Pass@word1", True),
-        pytest.param("qwerty", False),
-        pytest.param("Str@ng", False),
-        pytest.param("Pass@word", False),
-        pytest.param("Pass@1", False),
-        pytest.param("Password1", False),
-        pytest.param("pass@word1", False),
-        pytest.param("Pass@word1dfjsldfjlaskjdflsasdl", False)
+        ("Pass@word1", True),
+        ("qwerty", False),
+        ("Str@ng", False),
+        ("Pass@word", False),
+        ("Pass@1", False),
+        ("Password1", False),
+        ("pass@word1", False),
+        ("Pass@word1dfjsldfjlaskjdflsasdl", False)
     ]
 )
 def test_valid_password(new_input: str, expected_output: bool) -> None:
