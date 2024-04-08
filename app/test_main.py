b3050664d8ba4d64e92493529af8e8d6e8fd1789
evgenijnozdrenko <evgennozdrenko@MacBook-Pro.local>
2024-04-09 00:20:31 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        ("Pass@word1", True),
+        ("qwerty", False),
+        ("Str@ng", False),
+        ("S9r@ng", False),
+        ("Stttttgfgfgff88sdsdr@ng", False),
+        ("fass@word1", False),
+        ("PASDR8788@@D", True),
+        ("PASDR8788D", False),
+        ("PASDR@@@@D", False),
+    ],
+)
+def test_check_password(password: str, result: bool) -> None:
+    assert check_password(password) == result
