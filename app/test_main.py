93a5588bbdf5b9c463423f806725563cfe5b0c16
Volkov <volkovyegorandriiovich@gmail.com>
2024-04-19 22:47:59 +0300

    tests v1.0

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,40 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "test_string",
+    [
+        pytest.param(
+            "Pass@word1ї",
+            id="all letters should be within ASCII"
+        ),
+        pytest.param(
+            "Pass@w1",
+            id="length should be >= 8"
+        ),
+        pytest.param(
+            "Pass@word1qqqqqqq",
+            id="length should be <= 16"
+        ),
+        pytest.param(
+            "Pass@word",
+            id="must have at least 1 digit"
+        ),
+        pytest.param(
+            "Password1",
+            id="must have at least 1 special '$@#&!-_'"
+        ),
+        pytest.param(
+            "pass@word1",
+            id="must have at least 1 upper"
+        ),
+    ]
+)
+def test_wrong_pass(test_string: str) -> None:
+    assert check_password(password=test_string) is False
+
+
+def test_good_pass() -> None:
+    assert check_password(password="Pass@word1") is True
