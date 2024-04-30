15acb225def60cf1f76a61b41f60c272cfe78628
Liudmyla <4888814@gmail.com>
2024-04-30 14:03:44 +0300

    sol

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,22 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        pytest.param("1234", False, id="only numbers"),
+        pytest.param("loijug", False, id="no digits"),
+        pytest.param("loiju123g", False, id="without special symbols"),
+        pytest.param("qdjbyugu121dferfr2QQ!!!!", False, id="too long"),
+        pytest.param("1", False, id="min_length"),
+        pytest.param("aoAdvtf1@", True, id="correct"),
+        pytest.param("Jfnouyu19@", True, id="correct"),
+        pytest.param("short", False, id="too short"),
+        pytest.param("noppercase1!", False, id="no uppercase letters"),
+        pytest.param("8рш1_шкЦИ", False, id="no special_symbols"),
+        pytest.param("noDigitsAnd", False, id="no digits and no special characters")
+    ]
+)
+def test_password(password: str, result: bool) -> None:
+    assert check_password(password) == result
