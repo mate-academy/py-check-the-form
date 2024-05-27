3f9aa64f00cac4ebb1fa7b083ba5433e6acc5d04
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-05-28 00:36:01 +0300

    fix

--- app/test_main.py
+++ app/test_main.py
@@ -6,9 +6,15 @@ from app.main import check_password
 @pytest.mark.parametrize(
     "password, is_password",
     [
-        ("Pass@word1", True),
-        ("qwerty", False),
-        ("Str@ng", False)
+        ("PassWord_#", False),
+        ("123456789", False),
+        ("qweRty123", False),
+        ("Pass@word166", True),
+        ("Str@ng", False),
+        ("PASword12f434", False),
+        ("@pAS12", False),
+        ("sword45_@&-66", False),
+        ("Pa$s@_word66" * 4, False)
     ]
 )
 def test_check_password(password: str, is_password: bool) -> None:
