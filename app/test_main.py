486be1373ae11f7c7463ceebcc77491d7e07625c
AndrewwSSS <clash2clans1one@gmail.com>
2024-06-03 16:53:43 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,30 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    ("password, expected"),
+    [
+        ("Pass@word1", True),
+        ("P@1_fs", False),
+        ("Str@ng", False),
+        ("Pass@word1fsdffsf", False),
+        ("passw1@ord", False),
+        ("PF1_fsSDGSF", False),
+        ("PF@_fsSDGSF", False),
+
+    ],
+    ids=[
+        "Function should return True when all conditions complied with",
+        "Password should contains at least 8 characters",
+        ("Password should contains at least 1 digit,"
+         " 1 special character, 1 uppercase letter."),
+        "Password should contains 16 characters inclusive",
+        "Password should contains al leas 1 uppercase",
+        "Password should contains al leas 1 special symbol",
+        "Password should contains al leas 1 digit",
+    ]
+)
+def test_check_password(password: str, expected: bool) -> None:
+    assert check_password(password) == expected
