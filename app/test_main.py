4f168beb9d32fefcf0ab4393749a48b3658c0bd0
Vlad <vlad@example.com>
2024-06-08 21:52:57 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,18 +1,19 @@
 from app.main import check_password
 import pytest
 
+
 @pytest.mark.parametrize(
-        "password,result",
-        [
-            ("short", False),
-            ("longlonglonglonglonglong", False),
-            ("Pass@word1", True),
-            ("ауршщыаы", False),
-            ("password1", False),
-            ("12345678!", False),
-            ("Aa1!bbbb", True),
-            ("Aa1!bbbbcccccddd", True)
-        ]
+    "password,result",
+    [
+        ("short", False),
+        ("longlonglonglonglonglong", False),
+        ("Pass@word1", True),
+        ("ауршщыаы", False),
+        ("password1", False),
+        ("12345678!", False),
+        ("Aa1!bbbb", True),
+        ("Aa1!bbbbcccccddd", True)
+    ]
 )
-def test_check_password(password, result) -> None:
+def test_check_password(password: str, result: bool) -> None:
     assert check_password(password) == result
