2941072df39fdc835f2aef005d80fd8ad03c4a8e
Andrew Chugreev <andrey.4.1985@gmail.com>
2024-06-18 23:08:37 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,27 +4,29 @@ from app.main import check_password
 
 
 @pytest.mark.parametrize(
-    "password, result",
+    "password,result",
     [
         pytest.param(
-            "Pass@word1",
-            True,
-            id="Test valid password"
+            "Pass@word1", True,
+            id="Strong password 'Pass@word1'"
         ),
         pytest.param(
-            "qwerty",
-            False,
-            id="Test invalid password"
+            "Password", False,
+            id="Password should contain at least 1 special character"
         ),
         pytest.param(
-            "Str@ng",
-            False,
-            id="Test invalid password"
+            "q@3", False,
+            id="Password should contain at least 8 characters"
         ),
+        pytest.param(
+            "Pass@w00000000000000000000000rd", False,
+            id="Password should not be large that 16 symbols"
+        ),
+        pytest.param(
+            "Крутий@Пароль228", False,
+            id="Password should contain only Latin chars"
+        )
     ]
 )
-def test_check_password(
-        password: str,
-        result: bool
-) -> None:
+def test_check_password(password: str, result: bool) -> None:
     assert check_password(password) == result
