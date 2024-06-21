f9e2c2a7315b528b216feaec60e6ace427060c6d
Igor Kolisnyk <moonbosszzz@gmail.com>
2024-06-21 15:06:23 +0200

    The solution of the task

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected_result",
+    [
+        ("Pass@word1", True),
+        ("pass@word1", False),
+        ("Password1", False),
+        ("Pa$8", False),
+        ("Pass@word", False),
+        ("TheSrongerPass@word1", False)
+    ],
+    ids=[
+        "Should return True if password is valid",
+        "Should returns False for passwords without uppercase letter",
+        "Should returns False for passwords without special symbols",
+        "Should returns False for short passwords",
+        "Should returns False for passwords without digits",
+        "Should returns False for too long passwords"
+    ]
+)
+def test_should_return_correct_result(password: str,
+                                      expected_result: bool) -> None:
+    assert check_password(password) == expected_result
