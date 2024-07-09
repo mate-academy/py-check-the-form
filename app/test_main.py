dd5ffc531894020d0827560974868c6f1fc0b8ef
Максим Данилів <maksym.danyliv.23@pnu.edu.ua>
2024-07-09 14:19:47 +0300

    Solution 2.0

--- app/test_main.py
+++ app/test_main.py
@@ -12,27 +12,11 @@ from app.main import check_password
         ("Too_long_password1", False),
         ("no_capital_let", False),
         ("No_spec_char", False),
-        ("1Small!", False)
+        ("1Small!", False),
+        ("Пароль123!", False)
+
     ]
 
 )
 def test_check_password(password: str, validity: bool) -> None:
     assert check_password(password) == validity
-
-
-@pytest.mark.parametrize(
-    "password, expected_error",
-    [
-        (12, TypeError),
-        (12.548, TypeError),
-        ({12}, TypeError),
-        ([12], TypeError),
-    ]
-
-)
-def test_check_password_for_error(
-        password: str,
-        expected_error: Exception
-) -> None:
-    with pytest.raises(Exception):
-        check_password(password)
