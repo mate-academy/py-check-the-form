a79f4a5f019e0a6a177fb515269c67622ef17af0
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-07-26 13:05:28 +0300

    Tests implemented

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+import pytest
+
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password",
+    [
+        ("Abcdefg@"),
+        ("Abcdefg1"),
+        ("abcdefg1@"),
+        ("Abcdefg1@*"),
+        ("Ab1@bbb"),
+        ("Ab1@Ab1@Ab1@Ab1@A")
+    ]
+)
+def test_should_return_false_if_password_has_not_any_element(
+        password: str,
+) -> None:
+    assert (check_password(password)) is False
