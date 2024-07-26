7cdcd607a3dcc3698e413bd33a4e50f0a6387017
Stepan <oleksiukstepann@gmail.com>
2024-07-26 19:33:51 +0300

    last_solution_because_i_am_too_tired_for_another_one

--- app/test_main.py
+++ app/test_main.py
@@ -3,41 +3,20 @@ import pytest
 from app.main import check_password
 
 
-def test_should_return_false_if_len_of_password_is_incorrect(
-        password: str,
-) -> None:
-    assert check_password("") == False
-    assert check_password("1234567") == False
-    assert check_password("12345678901234567") == False
-
-
 @pytest.mark.parametrize(
-    "password",
+    "password, result",
     [
-        "asdfghjkl",
-        "abcdEFG1",
-        "$@#&!-_$@#&!-_",
-        "1234567890",
-        "12345$@#&!-_",
-        "asdfg$@#&!-_",
-        "asdfg1234567",
-        "чомуНеДержавною",
-    ],
-    ids=[
-        "only upper letters",
-        "only lower letters",
-        "only symbols",
-        "only not correct symbols"
-        "only numbers",
-        "numbers with symbols",
-        "letters with symbols",
-        "letters with numbers",
-        "not Latin alphabet"
+        ("Passw@ord1", True),
+        ("12Password", False),
+        ("Pass@word", False),
+        ("1234567@#$", False),
+        ("!@#$%%^&@#$", False),
+        ("Pass1@", False),
+        ("PassPas1@sPassPassPassPass", False)
     ]
 )
-def test_should_return_false_if_password_is_incorrect(
+def test_password_should_be_correct(
         password: str,
+        result: bool
 ) -> None:
-    assert check_password(password) == False, (
-        "Check password for upper letter, digit and special letter"
-    )
+    assert check_password(password) == result
