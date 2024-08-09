098e4e0bc08736920c76828edb184984be07a0ed
andreykolomiec <akolomiec982@gmail.com>
2024-08-09 11:57:09 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,13 @@
-# write your code here
+from app.main import check_password
+
+
+def test_check_password() -> None:
+    assert check_password("Pass@word1") is True
+    assert check_password("Pass@word1&word2") is True
+    assert check_password("Pa@word1") is True
+    assert check_password("P@word1") is False
+    assert check_password("Pass@word1&word21") is False
+    assert check_password("p@word1") is False
+    assert check_password("Password1") is False
+    assert check_password("Pass@word") is False
+    assert check_password("password") is False
