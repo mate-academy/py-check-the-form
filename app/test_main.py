839538a76091b2105ac43dfd7596b6350b070470
achalaidiuk <sasha.treyser90@gmail.com>
2024-08-13 11:56:34 +0300

    Task Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,36 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "checked_password,expected_result",
+    [
+        ("Valid123$", True),
+        ("Test@Password9", True),
+        ("Secure1@Password", True),
+        ("MyPass_2024", True),
+        ("Hello_World1", True)
+    ]
+)
+def test_for_true_passwords(
+        checked_password: str,
+        expected_result: bool
+) -> None:
+    assert check_password(checked_password) == expected_result
+
+
+@pytest.mark.parametrize(
+    "checked_password,expected_result",
+    [
+        ("short", False),
+        ("longpasswordwithnospecialcharacter1", False),
+        ("12345678", False),
+        ("validpassword$buttoolong123", False),
+        ("noUppercase1$", False)
+    ]
+)
+def test_for_false_passwords(
+        checked_password: str,
+        expected_result: bool
+) -> None:
+    assert check_password(checked_password) == expected_result
