6a7db6dea38df982d52043faab85698edd676050
Viktor Panasiuk <Viktor66462@gmail.com>
2024-08-26 23:08:36 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+import pytest
+from unittest.mock import Mock
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password,value",
+    [
+        ("Pa@word1", True),
+        ("P@woet1", False),
+        ("Pass@wordd", False),
+        ("pass@word1", False),
+        ("Pacvbss23dcv2344@", False)
+    ]
+)
+def test_check_password(password: Mock, value: Mock) -> None:
+    assert check_password(password) == value
