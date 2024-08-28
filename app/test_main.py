58c2cf0bb49efa487455710cb67dbfae746327c3
Serhii-Chubur <serhichubur15@gmail.com>
2024-08-28 21:39:26 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,25 @@
-# write your code here
+from app.main import check_password
+
+
+def test_too_long() -> None:
+    assert check_password("Str@ng1man19032409") is False
+
+
+def test_too_short() -> None:
+    assert check_password("Str@ng1") is False
+
+
+def test_no_digit() -> None:
+    assert check_password("Str@ngiee") is False
+
+
+def test_no_upper() -> None:
+    assert check_password("qwerty@24") is False
+
+
+def test_no_special() -> None:
+    assert check_password("Password1") is False
+
+
+def test_valid_password() -> None:
+    assert check_password("P@ssword1") is True
