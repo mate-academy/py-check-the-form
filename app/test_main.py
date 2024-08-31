f9607e246e5d976df671002e5c03a2ff794073f1
Arsen <arsen.test.mess@gmail.com>
2024-09-01 01:59:13 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+import pytest
+
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password,result",
+    [
+        pytest.param("Pa$$word1ы", False, id="invalid character"),
+        pytest.param("Pa$1", False, id="less than eight characters"),
+        pytest.param("Pa$$wordPa$$word1",
+                     False,
+                     id="more than eighteen characters"),
+        pytest.param("Pa$$wordP", False, id="missing digit"),
+        pytest.param("Password1", False, id="missing a special symbol"),
+        pytest.param("pa$$word1", False, id="missing a capital letter"),
+        pytest.param("Pa$$word1", True, id="valid password")
+    ]
+)
+def test_validity_of_different_passwords(password: str,
+                                         result: bool) -> None:
+    assert check_password(password) == result
