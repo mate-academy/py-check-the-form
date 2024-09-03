9c75cd97a04c5801a8c260dcc56aaf384a5d7122
Arthur <avers.target@gmail.com>
2024-09-03 11:21:42 +0200

    feat: Add parameterized test for password validation

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,21 @@
-# write your code here
+import pytest
+
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected",
+    [
+        ("Pass@rodk1", True),
+        ("password", False),
+        ("Str@ng", False),
+        ("qwerty", False),
+        ("onlylette!rs@oftheLati!n@alphabet", False),
+        ("PassWithoutDigit", False),
+        ("NoSpecialChar", False),
+        ("@42PaGecG!2", True),
+    ],
+)
+def test_check_password(password: str, expected: bool) -> bool:
+    assert check_password(password) == expected
