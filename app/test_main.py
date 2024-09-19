6941ad4131dd0bc34118f6c855f631589dc73e76
Gariman <garimanblack1@gmail.com>
2024-09-19 13:07:38 +0300

    Check Password solution +

--- app/test_main.py
+++ app/test_main.py
@@ -9,6 +9,11 @@ import pytest
         ("Pass@word1", True),
         ("qwerty", False),
         ("Str@ng", False),
+        ("Password!", False),
+        ("AnotherLongPassword!@#", False),
+        ("alllower123!", False),
+        ("NoSpecialChar1A", False),
+        ("Short1@", False),
     ]
 )
 def test_check_password(password: str, result: bool) -> None:
