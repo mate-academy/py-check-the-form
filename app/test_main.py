b378a27f505d02a4ebf4560016100f2a03bc4407
Nazar Makivchuk <nazar.makivchuk@gmail.com>
2024-09-29 13:52:19 +0300

    test passwords

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected",
+    [
+        ("Pass@word1", True),
+        ("qwerty", False),
+        ("Str@ng", False),
+        ("NoSpecialChar1", False),
+        ("NoDigit@", False),
+        ("nouppercase@1", False),
+        ("Pass_word12345!", True),
+        ("Short1@", False),
+        ("Valid1$Pass", True),
+        ("NodigitAndSpecial!", False),
+        ("UPPERCASE$1", True),
+        ("lowercaseonly@1", False),
+    ]
+)
+def test_check_password(password: str, expected: bool) -> None:
+    assert check_password(password) == expected
