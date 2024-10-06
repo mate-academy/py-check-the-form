684065c8779119474e98322f5f34998c6dcf0c6e
user <nikitaskrypets@gmail.com>
2024-10-06 14:53:26 +0300

    created tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    ("password", "expected"),
+    [
+        ("Pass@word1", True),
+        ("Qwert@yy", False),
+        ("Qwert@1", False),
+        ("qwert@1yy", False),
+        ("Qwert1yy", False),
+        ("So1#@methingveryveryverylong", False)
+    ]
+)
+def test_check_password(
+        password: str,
+        expected: bool,
+) -> None:
+    assert check_password(password) == expected
