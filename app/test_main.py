6f5611a719c9a43faade528570da42cce0145a4d
Deadmarvald <deadmarvald@gmail.com>
2024-10-08 15:51:10 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,42 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password,expected",
+    [
+        pytest.param(
+            "Pass@word1",
+            True,
+            id="should return True if the pass is valid"
+        ),
+        pytest.param(
+            "Pass@word",
+            False,
+            id="should return False if the pass does not contain a digit"
+        ),
+        pytest.param(
+            "Paaaaaaaaaaass@word1",
+            False,
+            id="should return False if the pass is too long"
+        ),
+        pytest.param(
+            "pass@word1",
+            False,
+            id="should return False if the pass doesnt contain upper-letter"
+        ),
+        pytest.param(
+            "P@ss1",
+            False,
+            id="should return False if the pass is too short"
+        ),
+        pytest.param(
+            "Password1",
+            False,
+            id="should return False if the pass does not contain a spec char"
+        ),
+    ]
+)
+def test_check_password(password: str, expected: bool) -> None:
+    assert check_password(password) is expected
