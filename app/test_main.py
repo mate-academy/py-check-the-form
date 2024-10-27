b91b7d06d5954b3af6c594b9e3921d18a1c3acdc
Oleksii Ivanov <oleks4335@gmail.com>
2024-10-27 15:15:00 +0100

    Add Check password solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,14 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected_result",
+    [
+        ("Pass@word1", True),
+        ("qwerty", False),
+        ("Str@ng", False),
+    ],
+)
+def test_check_password(password: str, expected_result: bool) -> None:
+    assert check_password(password) == expected_result
