0f9063cb93bc55c478a5e82d3ef88b6e44b040cd
popeye88 <aleksei.taran@gmail.com>
2024-11-03 14:30:46 +0200

    fix

--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,7 @@ from app.main import check_password
 
 
 @pytest.mark.parametrize(
-    'password,expected',
+    "password,expected",
     [
         ("Pass@word1", True),
         ("<PASSWORD>", False),
@@ -12,8 +12,8 @@ from app.main import check_password
         ("$Tr0ng", False),
         ("Password123!@#4561", False),
         ("password123!@#", False),
-        ("Password123", False)
-    ]
+        ("Password123", False),
+    ],
 )
 def test_check_password(password: str, expected: bool) -> None:
     assert check_password(password) == expected
