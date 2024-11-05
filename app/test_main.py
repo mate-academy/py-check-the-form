887f50a62de4bffc893d6e77157c6f344b8a9f34
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2024-11-05 21:35:39 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password",
+    (
+        "Pass@word12345xyz",
+        "he3low@rld",
+        "B0gd$n",
+        "__qwErTy__",
+        "ilovePython3000"
+    )
+)
+def test_check_password(password: str) -> None:
+    assert not check_password(password)
