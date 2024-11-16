3421cb13cc1f8ac5018fe0d492e540d0bff861cb
konstantinou77 <kostiakryvulia@gmail.com>
2024-11-16 14:30:29 +0200

    py-check-password solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,31 @@
-import pytest
 from app.main import check_password
 
-def test_check_password():
+
+def test_check_password() -> None:
     assert check_password("Pass@word1") is True
     assert check_password("Hello@123") is True
     assert check_password("Secure@99") is True
 
-def test_short_password():
+
+def test_short_password() -> None:
     assert check_password("Short@1") is False
 
-def test_long_password():
+
+def test_long_password() -> None:
     assert check_password("IsLong@password123") is False
 
-def test_no_digit():
+
+def test_no_digit() -> None:
     assert check_password("Nodigit@") is False
 
-def test_no_special_character():
+
+def test_no_special_character() -> None:
     assert check_password("NoSpec1al") is False
 
-def test_no_uppercase():
+
+def test_no_uppercase() -> None:
     assert check_password("n0uppercase@") is False
 
-def test_only_specials():
+
+def test_only_specials() -> None:
     assert check_password("@@@@@111") is False
