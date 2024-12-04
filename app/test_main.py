3ee361cf6619fc8765abd424944f85746c3591ce
Serhiy <sergeynechaenko@ukr.net>
2024-12-04 19:58:10 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+from app.main import check_password
+
+
+def test_should_return_false_if_pass_is_to_small_password() -> None:
+    password = "sd"
+    assert check_password(password) is True
+
+
+def test_should_return_false_if_pass_is_to_long() -> None:
+    password = "Password@123rwfwfwffwf"
+    assert check_password(password) is True
+
+
+def test_should_return_false_if_pass_doesnt_has_digit() -> None:
+    password = "Password@"
+    assert check_password(password) is True
+
+
+def test_should_return_false_if_pass_doesnt_has_special_simbols() -> None:
+    password = "Password123"
+    assert check_password(password) is True
+
+
+def test_should_return_false_if_pass_doesnt_has_upper_letter() -> None:
+    password = "assword@123"
+    assert check_password(password) is True
