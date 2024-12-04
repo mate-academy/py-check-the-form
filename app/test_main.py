24bf3b2e2dc2ee9036848b1aa3251c6ad052161c
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2024-12-04 07:47:34 +0100

    adding tests to test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,31 @@
-# write your code here
+from app.main import check_password
+
+
+def test_with_correct_name() -> None:
+    password = "Pass@word1"
+    assert check_password(password) is True
+
+
+def test_without_upper_letters() -> None:
+    password = "qwerty123@"
+    assert check_password(password) is False
+
+
+def test_without_digits() -> None:
+    password = "Str@ngPass"
+    assert check_password(password) is False
+
+
+def test_with_too_long_password() -> None:
+    password = "Str@ng12345678912345"
+    assert check_password(password) is False
+
+
+def test_with_too_short_password() -> None:
+    password = "Pass@1"
+    assert check_password(password) is False
+
+
+def test_without_special_letters() -> None:
+    password = "Password"
+    assert check_password(password) is False
