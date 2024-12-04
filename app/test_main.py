a99bb0d3a6671de5bc22b73b8f19f6ec03ae52b6
Serhiy <sergeynechaenko@ukr.net>
2024-12-04 20:22:12 +0200

    added checking true for all tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,26 +1,38 @@
 from app.main import check_password
 
 
-def test_should_return_false_if_pass_is_to_small_password() -> None:
-    password = "sd"
+def test_should_check_min_length() -> None:
+    password = "s"
     assert check_password(password) is False
+    password = "jd&H12"
+    assert check_password(password) is False
+    password = ("Pasord@2")
+    assert check_password(password) is True
 
 
 def test_should_return_false_if_pass_is_to_long() -> None:
     password = "Password@123rwfwfwffwf"
     assert check_password(password) is False
+    password = ("Password@123")
+    assert check_password(password) is True
 
 
-def test_should_return_false_if_pass_doesnt_has_digit() -> None:
+def test_should_return_false_if_pass_doesnt_have_digit() -> None:
     password = "Password@"
     assert check_password(password) is False
+    password = ("Password@123")
+    assert check_password(password) is True
 
 
-def test_should_return_false_if_pass_doesnt_has_special_simbols() -> None:
+def test_should_return_false_if_pass_doesnt_have_special_simbols() -> None:
     password = "Password123"
     assert check_password(password) is False
+    password = ("Password@123")
+    assert check_password(password) is True
 
 
-def test_should_return_false_if_pass_doesnt_has_upper_letter() -> None:
+def test_should_return_false_if_pass_doesnt_have_upper_letter() -> None:
     password = "assword@123"
     assert check_password(password) is False
+    password = ("Password@123")
+    assert check_password(password) is True
