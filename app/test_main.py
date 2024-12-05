6668f18955b2dc52e191056efd2564be801ae77c
Beliar24 <blackdid7@gmail.com>
2024-12-05 02:05:54 +0200

    fix

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,15 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, answer",
+    [
+        ("Pass@word1", True),
+        ("qwerty", False),
+        ("Str@ng", False)
+    ]
+)
+def test_change_password(password: str, answer: bool) -> None:
+    assert check_password(password) is answer
