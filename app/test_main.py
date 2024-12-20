00f4e2f5677476852a898929c881e433c783b908
Vova <vova.gld05@gmail.com>
2024-12-20 19:32:49 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+import pytest
+
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        ("Short1!", False),
+        ("ThisIsWayTooLong1!", False),
+        ("password1!", False),
+        ("Password!", False),
+        ("Password1", False),
+        ("Password1*", False),
+        ("Пароль1!#", False),
+        ("Strong_Pass1!", True)
+    ]
+)
+def test_check_all_validators(
+        password: str,
+        result: bool
+) -> None:
+    assert check_password(password) == result
