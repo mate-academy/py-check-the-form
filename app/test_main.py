30ea4ce038dbee7e4b8c797366aa251293701a39
JustVov4uk <volodabudzan4@gmail.com>
2025-01-09 15:44:02 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,37 @@
-# write your code here
+from app.main import check_password
+
+
+def test_for_true_password():
+    assert check_password("Password_1")
+
+
+def test_without_a_capital_letter():
+    assert not check_password("password_1")
+
+
+def test_without_a_number():
+    assert not check_password("Password_")
+
+
+def test_without_a_special_character():
+    assert not check_password("Password1")
+
+
+def test_less_than_8_characters():
+    assert not check_password("Pass_1")
+
+
+def test_more_than_16_characters():
+    assert not check_password("ItismyPassword_11")
+
+
+def test_misfits_characters():
+    assert not check_password("Password_1++")
+
+
+def test_when_only_numbers():
+    assert not check_password("357348573")
+
+
+def test_password_with_spaces():
+    assert not check_password("Pass word_1")
