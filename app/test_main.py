36b1eca56f679e02af44461be4c7804f04a59899
Taras <t.kudinov90@gmail.com>
2025-01-09 13:44:42 +0200

    Created four test functions for main function tests. Internal tests passed.

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+from app.main import check_password
+
+
+def test_password_length_min() -> None:
+    assert check_password(password="drW321@") is False
+
+
+def test_password_length_max() -> None:
+    assert check_password(password="DFGKkwfmwk23234!@#") is False
+
+
+def test_password_symbols() -> None:
+    assert check_password(password="Aіке2345!") is False
+
+
+def test_password_conditions() -> None:
+    assert check_password(password="Svit2307@")
